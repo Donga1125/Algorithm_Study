15fc3f32925161fed086aeb7b4d6a6edfce67483
hae02y <godud1118@gmail.com>
2023-10-13 11:30:04 +0900

    algo: 10808/알파벳개수

---
+++ 기초1/Day04/정해영/알파벳개수.java
@@ -0,0 +1,27 @@
+package 기초1.Day04.정해영;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+public class 알파벳개수 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine();
+
+        int[] arr = new int['z'-'a' + 1];
+
+        for(int i=0; i<str.length(); i++){
+            arr[str.charAt(i) - 'a']++;
+        }
+
+        Arrays.stream(arr).forEach(a -> System.out.printf("%d ", a));
+        br.close();
+    }
+}
