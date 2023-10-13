3061c781c0ae8c0c2f6f24deafd22e116ac7a2fa
Donga1125 <ehddk0133@naver.com>
2023-10-13 12:03:35 +0900

    algo:10808/알파벳 개수

---
+++ 기초1/Day04/정동아/알파벳개수.java
@@ -0,0 +1,28 @@
+package 기초1.Day04.정동아;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 알파벳개수 {
+    //각 알파벳이 단어에 몇개 포함되어있는지 출력
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuffer sb = new StringBuffer();
+
+        String input = br.readLine(); //문자 넣어줌
+        int [] result = new int[26]; //소문자만 들어가니까 26개 배열 생성
+
+        for (int i =0; i< input.length(); i++){
+            int index = input.charAt(i) - (int)'a'; //int형으로 형변환한 a를 뺀 값으로 알파벳 인덱스 위치로 지정
+            result[index]++;
+        }
+
+        for (int i =0; i< result.length; i++){
+            sb.append(result[i]).append(" ");
+        }
+        br.close();
+        System.out.println(sb);
+    }
+}
