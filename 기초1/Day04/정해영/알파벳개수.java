1aac5206cf4ea2b17be541b866932d4340b66af0
hae02y <godud1118@gmail.com>
2023-10-16 08:27:47 +0900

    algo: 알파벳개수

--- 기초1/Day04/정해영/알파벳개수.java
+++ 기초1/Day04/정해영/알파벳개수.java
@@ -4,8 +4,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
 
 public class 알파벳개수 {
 
