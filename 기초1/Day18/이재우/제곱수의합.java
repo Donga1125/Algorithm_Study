34cb3a171eb005abcfad7f8b9ecbde040deca74a
leesaewoo <ljww123@naver.com>
2023-11-06 17:03:04 +0900

    comment: 주석 추가

--- 기초1/Day18/이재우/제곱수의합.java
+++ 기초1/Day18/이재우/제곱수의합.java
@@ -11,6 +11,8 @@ public class 제곱수의합 {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(bufferedReader.readLine());
+
+        // 배열의 인덱스를 제곱수들의 합으로 표현할 떄 그 항의 최소개수를 해당 인덱스의 요소로 갖는 배열
         int[] numberOfSquares = new int[n + 1];
 
         for(int i = 1 ; i < n + 1 ; i++) {
@@ -18,6 +20,7 @@ public class 제곱수의합 {
 
             for(int j = 1 ; i >= j * j ; j++) {
                 if(numberOfSquares[i] >= numberOfSquares[j * j] + numberOfSquares[i - j * j]) {
+                    //점화식
                     numberOfSquares[i] = numberOfSquares[j * j] + numberOfSquares[i - j * j];
                 }
 
