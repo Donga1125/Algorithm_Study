bd755b468702334a072c0d3e7a9b2d629fa10f5a
Ernest45 <readonme@naver.com>
2023-11-08 20:04:56 +0900

    algo:동물원,오르막수

--- 기초1/Day19/이재우/문제123더하기3.java
+++ 기초1/Day19/이재우/문제123더하기3.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class 일이삼더하기3 {
+public class 문제123더하기3 {
 
     public static void main(String[] args) throws IOException {
 
