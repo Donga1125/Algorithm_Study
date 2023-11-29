1d38f87b18d68d7757df39657d5175d818ba025c
leesaewoo <ljww123@naver.com>
2023-11-29 10:10:49 +0900

    algo: N과M1~6

---
+++ 기초2/Day31/이재우/N과M1.java
@@ -0,0 +1,53 @@
+package 기초2.Day31.이재우;
+
+import java.io.*;
+
+public class N과M1 {
+
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    private static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    private static int[] arr;
+    private static boolean[] visit;
+    private static int n;
+    private static int m;
+
+    public static void main(String[] args) throws IOException {
+
+        String[] input = br.readLine().split(" ");
+        n = Integer.parseInt(input[0]);
+        m = Integer.parseInt(input[1]);
+        arr = new int[m];
+        visit = new boolean[n];
+
+        for(int i = 0 ; i < n ; i++) {
+            DFS(i, 0);
+        }
+
+        bw.close();
+    }
+
+    private static void DFS(int number, int count) throws IOException {
+
+        visit[number] = true;
+
+        arr[count] = number + 1;
+        if(m == count + 1){
+            for(int i = 0 ; i < m ; i++) {
+                bw.write(arr[i] + " ");
+            }
+            bw.write("\n");
+            visit[number] = false;
+            return;
+        }
+
+        for(int i = 0 ; i < n ; i++){
+            if(!visit[i]){
+                DFS(i, count + 1);
+            }
+        }
+
+        visit[number] = false;
+    }
+}
---
+++ 기초2/Day33/이재우/N과M5.java
@@ -0,0 +1,58 @@
+package 기초2.Day33.이재우;
+
+import java.io.*;
+import java.util.Arrays;
+
+public class N과M5 {
+
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    private static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    private static int[] arr;
+    private static int[] answer;
+    private static boolean[] visit;
+    private static int n;
+    private static int m;
+
+    public static void main(String[] args) throws IOException {
+
+        String[] input = br.readLine().split(" ");
+        n = Integer.parseInt(input[0]);
+        m = Integer.parseInt(input[1]);
+        arr = new int[n];
+        answer = new int[m];
+        visit = new boolean[n];
+
+        String[] strArr = br.readLine().split(" ");
+        for(int i = 0 ; i < n ; i++) {
+            arr[i] = Integer.parseInt(strArr[i]);
+        }
+
+        Arrays.sort(arr);
+
+        DFS(0);
+
+        bw.close();
+    }
+
+    private static void DFS(int count) throws IOException {
+
+        if(count == m) {
+            for(int i = 0 ; i < answer.length ; i++) {
+                bw.write(answer[i] + " ");
+            }
+            bw.write("\n");
+            return;
+        }
+
+        for(int i = 0 ; i < n ; i++) {
+            if(!visit[i]) {
+                visit[i] = true;
+                answer[count] = arr[i];
+                DFS(count + 1);
+                visit[i] = false;
+            }
+        }
+    }
+}
