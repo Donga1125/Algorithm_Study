49cd321e6f1ebe56749a350cd0fa2119d1a228a8
Ernest45 <gkgkswns71@gmail.com>
2024-01-23 22:04:02 +0900

    1157 단어공부

--- 알고리즘/단어공부/SolutionArrays1157.java
+++ 알고리즘/단어공부/SolutionArrays1157.java
@@ -1,7 +1,6 @@
 package 알고리즘.단어공부;
 
 import java.io.*;
-import java.util.ArrayList;
 //문제
 //알파벳 대소문자로 된 단어가 주어지면, 이 단어에서 가장 많이 사용된 알파벳이 무엇인지 알아내는 프로그램을 작성하시오. 단, 대문자와 소문자를 구분하지 않는다.
 //
@@ -11,7 +10,7 @@ import java.util.ArrayList;
 //출력
 //첫째 줄에 이 단어에서 가장 많이 사용된 알파벳을 대문자로 출력한다. 단, 가장 많이 사용된 알파벳이 여러 개 존재하는 경우에는 ?를 출력한다.
 
-public class SolutionArrays {
+public class SolutionArrays1157 {
 
 
     //    public char Solution(String str) throws IOException {
