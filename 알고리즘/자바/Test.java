d732e986965f38b2baf13e562b9012718552b9df
Ernest45 <gkgkswns71@gmail.com>
2024-01-26 21:50:27 +0900

    Test

--- 알고리즘/자바/Test.java
+++ 알고리즘/자바/Test.java
@@ -1,2 +1,13 @@
-package 알고리즘.자바;public class Test {
-}
+package 알고리즘.자바;
+
+public class Test {
+    public static void main(String[] args) {
+
+
+
+    String str = new String(" Java     ");
+System.out.println("문자열 : " + str);
+System.out.println(str + '|');
+System.out.println(str.trim() + '|');
+System.out.println("trim() 메서드 호출 후 문자열 : " + str);
+}}
