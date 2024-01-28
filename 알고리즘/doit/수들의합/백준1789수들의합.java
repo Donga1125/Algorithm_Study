91afb91453088426dfaff385daed33e27cbaf260
Ernest45 <gkgkswns71@gmail.com>
2024-01-28 22:47:12 +0900

    백준1789 수들의합 미완성

---
+++ 알고리즘/doit/수들의합/백준1789수들의합.java
@@ -0,0 +1,41 @@
+package 알고리즘.doit.수들의합;
+
+import java.util.Scanner;
+
+//수들의 합
+//시간 제한	메모리 제한	제출	정답	맞힌 사람	정답 비율
+//2 초	128 MB	58571	23890	20234	41.667%
+//문제
+//서로 다른 N개의 자연수의 합이 S라고 한다. S를 알 때, 자연수 N의 최댓값은 얼마일까?
+//
+//입력
+//첫째 줄에 자연수 S(1 ≤ S ≤ 4,294,967,295)가 주어진다.
+//
+//출력
+//첫째 줄에 자연수 N의 최댓값을 출력한다.
+
+// 이건 투포인터가 아니라 처음부터 1부터 더해가며 그 조합으로 s를 만들어야 하는데, 만들면 그 술 리턴
+public class 백준1789수들의합 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        long N = sc.nextLong();
+        int sum = 1, count = 1;
+
+        while (sum <= N) {
+
+            if (sum == N) {
+                break;}
+            count++;
+            sum = sum + count;
+
+
+
+            }
+
+        }
+
+
+    }
+
