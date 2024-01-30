9cd991d981d79b3d9d911664af31869845bfd09b
Ernest45 <gkgkswns71@gmail.com>
2024-01-30 16:21:37 +0900

    개출력

---
+++ 알고리즘/doit/백준3273두수의합.java
@@ -0,0 +1,2 @@
+package 알고리즘.doit;public class 백준3273두수의합 {
+}
---
+++ 알고리즘/단계별백준/입출력과사칙연산/개.java
@@ -0,0 +1,18 @@
+package 알고리즘.단계별백준.입출력과사칙연산;
+
+public class 개 {
+    public static void main(String[] args) {
+
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+    }
+
+    //|\_/|
+    //|q p|   /}
+    //( 0 )"""\
+    //|"^"`    |
+    //||_/=\\__|
+}
