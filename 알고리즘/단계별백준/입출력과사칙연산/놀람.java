7b135b7e5b7d4a213ba023aa170a9244a5f55d99
Ernest45 <gkgkswns71@gmail.com>
2024-01-30 15:17:04 +0900

    사칙연산 놀람

---
+++ 알고리즘/단계별백준/입출력과사칙연산/놀람.java
@@ -0,0 +1,15 @@
+package 알고리즘.단계별백준.입출력과사칙연산;
+
+import java.util.Scanner;
+
+public class 놀람 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        StringBuilder sb = new StringBuilder();
+        String answer = String.valueOf(sb.append(s).append("??!"));
+
+        System.out.println(answer);
+    }
+}
