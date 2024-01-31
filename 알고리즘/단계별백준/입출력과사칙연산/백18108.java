db1fe34fd3b39b39f60c62b143dfbd27c00dc3a0
Ernest45 <gkgkswns71@gmail.com>
2024-01-31 21:39:14 +0900

    1998년생인내가태국에선?

---
+++ 알고리즘/단계별백준/입출력과사칙연산/백18108.java
@@ -0,0 +1,15 @@
+package 알고리즘.단계별백준.입출력과사칙연산;
+
+import java.util.Scanner;
+
+public class 백18108 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int y = sc.nextInt();
+
+        System.out.println(y - 543);
+    }
+}
