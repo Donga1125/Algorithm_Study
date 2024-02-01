06e1d10bea29267897491fb729d6009284c7f696
Ernest45 <gkgkswns71@gmail.com>
2024-02-01 20:28:20 +0900

    a플b4

--- 알고리즘/단계별백준/반복문/a플b4.java
+++ 알고리즘/단계별백준/반복문/a플b4.java
@@ -1,2 +1,24 @@
-package 알고리즘.단계별백준.반복문;public class a플b4 {
+package 알고리즘.단계별백준.반복문;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class a플b4 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int answer = 0;
+        String line;
+
+        while ((line = br.readLine()) != null && !line.isEmpty()) {
+
+            StringTokenizer st = new StringTokenizer(line);
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            answer = a + b;
+            System.out.println(answer);
+        }
+    }
 }
