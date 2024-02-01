5514dd2afcfc808ae20e904282106338262795a5
Ernest45 <gkgkswns71@gmail.com>
2024-02-01 20:27:02 +0900

    a플b5

--- 알고리즘/단계별백준/반복문/A플b5.java
+++ 알고리즘/단계별백준/반복문/A플b5.java
@@ -1,24 +1,28 @@
 package 알고리즘.단계별백준.반복문;
 
 import java.io.BufferedReader;
-    import java.io.IOException;
-    import java.io.InputStreamReader;
-    import java.util.StringTokenizer;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 
-    public class A플b8 {
-        public static void main(String[] args) throws IOException {
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            int answer = 0;
-            int t = Integer.parseInt(br.readLine());
+public class A플b5 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int answer = 0;
+        String line;
 
-            for (int i = 1; i <= t; i++) {
-                StringTokenizer st = new StringTokenizer(br.readLine());
-                int a = Integer.parseInt(st.nextToken());
-                int b = Integer.parseInt(st.nextToken());
+        while ((line = br.readLine()) != null && !line.isEmpty()) {
 
-                answer = a + b;
-                System.out.println(String.format("Case #%d: %d + %d = %d", i, a, b, answer));
-            }            }
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            answer = a + b;
+            System.out.println(answer);
         }
+    }
+
+        
+}
 
