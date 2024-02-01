3f5130d21e32423b6d065257bd95e29f63e1573e
Ernest45 <gkgkswns71@gmail.com>
2024-02-01 20:26:15 +0900

    별찎기

---
+++ 알고리즘/단계별백준/반복문/a플b4.java
@@ -0,0 +1,2 @@
+package 알고리즘.단계별백준.반복문;public class a플b4 {
+}
---
+++ 알고리즘/단계별백준/반복문/별찍기.java
@@ -0,0 +1,20 @@
+package 알고리즘.단계별백준.반복문;
+
+import java.util.Scanner;
+
+public class 별찍기 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+
+            }
+
+            System.out.println();
+        }
+    }
+}
