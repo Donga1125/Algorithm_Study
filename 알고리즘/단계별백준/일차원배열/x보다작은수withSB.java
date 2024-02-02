d9de3d17d87ffea75cc6095827f8626ca41c9252
Ernest45 <gkgkswns71@gmail.com>
2024-02-02 22:16:22 +0900

    x보다 작은수WithSB

--- 알고리즘/단계별백준/일차원배열/x보다작은수withSB.java
+++ 알고리즘/단계별백준/일차원배열/x보다작은수withSB.java
@@ -1,28 +1,29 @@
 package 알고리즘.단계별백준.일차원배열;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
-public class x보다작은수 {
+public class x보다작은수withSB {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        StringBuilder sb = new StringBuilder();
 
         int n = sc.nextInt();
         int x = sc.nextInt();
 
         int[] arr = new int[n];
-        String answer = "";
+        String answer = " ";
 
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
             if (arr[i] < x) {
-                answer = answer + String.valueOf(arr[i] + " ");
+                sb.append(arr[i]).append(" ");
 
             }
         }
 
-        System.out.println(answer);
+        System.out.println(sb.toString().trim());
 
     }
 }
