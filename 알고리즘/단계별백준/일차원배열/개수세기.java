c356cf9c2ff609cdcc2b40bbe674f53ab9ac7c66
Ernest45 <gkgkswns71@gmail.com>
2024-02-02 22:15:32 +0900

    개수세기

--- 알고리즘/단계별백준/일차원배열/개수세기.java
+++ 알고리즘/단계별백준/일차원배열/개수세기.java
@@ -1,4 +1,32 @@
 package 알고리즘.단계별백준.일차원배열;
 
-public class 숫자세기 {
+import java.util.Scanner;
+
+public class 개수세기 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int answer = 0;
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int target = sc.nextInt();
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target)
+                answer++;
+
+        }
+
+        System.out.println(answer);
+
+    }
 }
+
+
