6d9ac65fea3fe05c81451343b2e9c033121e8bbc
Ernest45 <gkgkswns71@gmail.com>
2024-02-02 22:15:49 +0900

    x보다작은수

--- 알고리즘/단계별백준/일차원배열/x보다작은수.java
+++ 알고리즘/단계별백준/일차원배열/x보다작은수.java
@@ -1,2 +1,28 @@
-package 알고리즘.단계별백준.일차원배열;public class x보다작은수 {
+package 알고리즘.단계별백준.일차원배열;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class x보다작은수 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int x = sc.nextInt();
+
+        int[] arr = new int[n];
+        String answer = "";
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+            if (arr[i] < x) {
+                answer = answer + String.valueOf(arr[i] + " ");
+
+            }
+        }
+
+        System.out.println(answer);
+
+    }
 }
