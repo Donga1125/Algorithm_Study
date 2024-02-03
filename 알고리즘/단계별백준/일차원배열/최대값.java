c36c9c43e8fd31811ddf8d624def213a1b351fa3
Ernest45 <readonme@naver.com>
2024-02-04 04:22:46 +0900

    최대값 최종

--- 알고리즘/단계별백준/일차원배열/최대값.java
+++ 알고리즘/단계별백준/일차원배열/최대값.java
@@ -1,24 +1,24 @@
 package 알고리즘.단계별백준.일차원배열;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class 최대값 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int arr[] = new int[n];
+        int arr[] = new int[9];
         int max = 0;
         int idx = 0;
 
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < arr.length; i++) {
             arr[i] = sc.nextInt();
             if (max <= arr[i]) {
                 max = arr[i];
+                idx = i;
 
             }
         }
         System.out.println(max);
+        System.out.println(idx + 1);
     }
 
 }
