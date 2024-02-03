f882d347de948a9c7ce508da77661339572ff346
Ernest45 <readonme@naver.com>
2024-02-03 23:19:22 +0900

    최대값

---
+++ 알고리즘/단계별백준/일차원배열/최대값.java
@@ -0,0 +1,18 @@
+package 알고리즘.단계별백준.일차원배열;
+
+import java.util.Scanner;
+
+public class 최대값 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        int max = 0;
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+            if(max =<)
+        }
+    }
+
+}
