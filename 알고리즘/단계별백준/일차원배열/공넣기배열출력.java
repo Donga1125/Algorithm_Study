8ee462d4796fc01106c7ed89b7b3b98eb8624590
Ernest45 <readonme@naver.com>
2024-02-05 00:56:33 +0900

    공넣기 배열출력

---
+++ 알고리즘/단계별백준/일차원배열/공넣기배열출력.java
@@ -0,0 +1,39 @@
+package 알고리즘.단계별백준.일차원배열;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class 공넣기배열출력 {
+    // 들어온 값들을 계속 교체해주면 되는거
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[] arr = new int[n];
+        String answer = "";
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken()) - 1;
+            int end = Integer.parseInt(st.nextToken()) - 1;
+            int idx = Integer.parseInt(st.nextToken());
+            for (; start <= end ; start++) {
+                arr[start] = idx;
+
+            }
+        }
+        for (int value : arr) {
+            answer += value + " ";
+
+        }
+        System.out.println(answer);
+
+
+    }
+}
