2a20a2464f51af0ad5201537c471ce8bdefbe5ca
Ernest45 <readonme@naver.com>
2024-02-05 03:07:12 +0900

    과제안내신분어레이리스트

---
+++ 알고리즘/단계별백준/일차원배열/과제안내신분어레이리스트.java
@@ -0,0 +1,31 @@
+package 알고리즘.단계별백준.일차원배열;
+
+import java.util.*;
+
+
+public class 과제안내신분어레이리스트 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Integer> arrList = new ArrayList<>();
+
+
+        for (int i = 0; i < 28; i++) {
+            arrList.add(sc.nextInt());
+        }
+        Collections.sort(arrList);
+
+        for (int i = 1; i <= 30; i++) {
+            if (!arrList.contains(i)) {
+                System.out.println(i);
+
+            }
+
+
+        }
+
+
+
+        sc.close();
+    }
+}
