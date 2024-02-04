d78cbe94977bf1aae9132c18a3a00ac7f65761d4
Ernest45 <readonme@naver.com>
2024-02-05 03:28:20 +0900

    과제안내신분배열

--- 알고리즘/단계별백준/일차원배열/과제안내신분배열.java
+++ 알고리즘/단계별백준/일차원배열/과제안내신분배열.java
@@ -1,28 +1,31 @@
 package 알고리즘.단계별백준.일차원배열;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
 
 
-public class 과제안내신분 {
+public class 과제안내신분배열 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        ArrayList<Integer> arrList = new ArrayList<>();
-        
+
+        int arr[] = new int[30 + 1];
+        int student = 0;
 
         for (int i = 0; i < 28; i++) {
-            arrList.add(sc.nextInt());
+           student = sc.nextInt();
+            arr[student] = 1;
         }
-        Collections.sort(arrList);
 
-        for (int i = 1; i <= 30; i++) {
-            if (!arrList.contains(i)) {
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] != 1) {
                 System.out.println(i);
-
             }
-
-
         }
+
 
 
 
