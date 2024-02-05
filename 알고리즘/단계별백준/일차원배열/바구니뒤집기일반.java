96a7f1aa22c885aef014d8f837d37db2826c0790
Ernest45 <gkgkswns71@gmail.com>
2024-02-05 22:51:42 +0900

    바구니뒤집기일반

---
+++ 알고리즘/단계별백준/일차원배열/바구니뒤집기일반.java
@@ -0,0 +1,55 @@
+package 알고리즘.단계별백준.일차원배열;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 바구니뒤집기일반 {
+
+    public static void main(String[] args) throws IOException {
+
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[n];
+        int[] answer = new int[n];
+
+        for (int i = 1; i <= n; i++) {
+            arr[i - 1] = i;
+            answer[i - 1] = i;
+        }
+
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+
+            reverseArrays(arr, start -1, end -1);
+            }
+
+        for (int values : arr) {
+            System.out.print(values + " ");
+
+        }
+        }
+
+    public static void reverseArrays(int[] arr, int start, int end) {
+        while (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+
+        }
+    }
+
+    }
+
+
