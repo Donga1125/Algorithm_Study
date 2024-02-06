70080181e6331f9bb3a763279c257b6506049544
Ernest45 <gkgkswns71@gmail.com>
2024-02-06 20:15:42 +0900

    아스키코드

---
+++ 알고리즘/단계별백준/일차원배열/아스키코드.java
@@ -0,0 +1,18 @@
+package 알고리즘.단계별백준.일차원배열;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class 아스키코드 {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        int a = s.charAt(0);
+
+//        int a = System.in.read();
+
+
+        System.out.println(a);
+
+    }
+}
