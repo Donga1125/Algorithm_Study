d3b9228d1234a6525714c04714937d582761ab43
Ernest45 <gkgkswns71@gmail.com>
2024-02-06 21:11:06 +0900

    문자열반복

---
+++ 알고리즘/단계별백준/일차원배열/문자열반복.java
@@ -0,0 +1,35 @@
+package 알고리즘.단계별백준.일차원배열;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 문자열반복 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < t; i++) {
+
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int r = Integer.parseInt(st.nextToken());
+            String str = st.nextToken();
+            String answer = "";
+
+            for (int j = 0; j < str.length(); j++) {
+
+                for (int k = 0; k < r; k++) {
+                    answer = answer + str.charAt(j);
+
+                }
+            }
+            System.out.println(answer);
+        }
+
+
+    }
+}
