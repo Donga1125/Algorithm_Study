27b5452d6d0e92b840484daf9e56db2346b9fc5f
Ernest45 <gkgkswns71@gmail.com>
2024-02-06 22:05:37 +0900

    단어의개수 trim실수

--- 알고리즘/단계별백준/일차원배열/단어의개수기본.java
+++ 알고리즘/단계별백준/일차원배열/단어의개수기본.java
@@ -1,2 +1,18 @@
-package 알고리즘.단계별백준.일차원배열;public class 단어의개수기본 {
-}
+package 알고리즘.단계별백준.일차원배열;
+
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class 단어의개수기본 {
+
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        sc.close();
+
+        StringTokenizer st = new StringTokenizer(str, " ");
+        System.out.println(st.countTokens());
+
+    }}
