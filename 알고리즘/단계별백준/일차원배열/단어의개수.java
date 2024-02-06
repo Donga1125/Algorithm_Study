14fa02d98c695aaf07391a0ebbe84cb2ce7d3107
Ernest45 <gkgkswns71@gmail.com>
2024-02-06 22:02:04 +0900

    단어의개수 trim()의숨은부분

---
+++ 알고리즘/단계별백준/일차원배열/단어의개수.java
@@ -0,0 +1,24 @@
+package 알고리즘.단계별백준.일차원배열;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class 단어의개수 {
+
+    //공백을 만나면 카운트를 올려주자
+
+    public static void main(String[] args) { // trim은 앞뒤 공백 다 제거
+
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+
+        str = str.trim(); // 다 제거했을때 0이 들어오면 0으로 반환해야하지만 1로 반환함
+        if (str == "") {
+            System.out.println(0);
+
+        } else {
+        String[] arr = str.split(" ");
+
+        System.out.println(arr.length);}
+
+    }}
