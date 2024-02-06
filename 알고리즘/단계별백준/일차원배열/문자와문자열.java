77b9f719b7bbaadddff44b82fae019b044023f9c
Ernest45 <gkgkswns71@gmail.com>
2024-02-06 19:17:39 +0900

    문자와 문자열

---
+++ 알고리즘/단계별백준/일차원배열/문자와문자열.java
@@ -0,0 +1,14 @@
+package 알고리즘.단계별백준.일차원배열;
+
+import java.util.Scanner;
+
+public class 문자와문자열 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        int n = sc.nextInt();
+
+        System.out.println(str.charAt(n - 1));
+    }
+}
