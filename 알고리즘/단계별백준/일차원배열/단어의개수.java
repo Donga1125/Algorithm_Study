dcd566fdc4076db0c9b5114b4fe04025c5778602
Ernest45 <gkgkswns71@gmail.com>
2024-02-06 22:05:20 +0900

    단어의개수ST

--- 알고리즘/단계별백준/일차원배열/단어의개수.java
+++ 알고리즘/단계별백준/일차원배열/단어의개수.java
@@ -7,12 +7,12 @@ public class 단어의개수 {
 
     //공백을 만나면 카운트를 올려주자
 
-    public static void main(String[] args) { // trim은 앞뒤 공백 다 제거
+    public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
         String str = sc.nextLine();
 
-        str = str.trim(); // 다 제거했을때 0이 들어오면 0으로 반환해야하지만 1로 반환함
+        str = str.trim(); // 다 제거했을때 0이 들어오면 0으로 반환해야하지만 1로 반환함 오류
         if (str == "") {
             System.out.println(0);
 
---
+++ 알고리즘/단계별백준/일차원배열/단어의개수기본.java
@@ -0,0 +1,2 @@
+package 알고리즘.단계별백준.일차원배열;public class 단어의개수기본 {
+}
