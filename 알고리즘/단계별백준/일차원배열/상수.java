24eee8f3e16c8765c94ab542bbea6a32acc32204
Ernest45 <readonme@naver.com>
2024-02-07 23:59:37 +0900

    상수리버스없이고치기

--- 알고리즘/단계별백준/일차원배열/상수.java
+++ 알고리즘/단계별백준/일차원배열/상수.java
@@ -16,6 +16,7 @@ public class 상수 {
         String m = String.valueOf(mm);
 
 
+
         StringBuilder sb = new StringBuilder();
 
         int a = 0;
@@ -23,22 +24,26 @@ public class 상수 {
         int b = 0;
 
 
-        for (int i = 0; i < n.length(); i++) {
+        for (int i = 2; i <= 0; i--) {
 //            n = n + String.valueOf(sb.append(n.charAt(i)));
-            sb = sb.append(sb).append(n.charAt(i));
-            a = Integer.parseInt(String.valueOf(sb));
+            sb = sb.append(n.charAt(i));
+            System.out.println(sb);
+
         }
+        a = Integer.parseInt(String.valueOf(sb));
 
         StringBuilder s6 = new StringBuilder();
 
-        for (int i = 0; i < m.length(); i++) {
+        for (int i = 2; i <=0; i--) {
 
 
 //            m = m + String.valueOf(sb.append(m.charAt(i)));
-            s6 = s6.append(s6).append(m.charAt(i));
-            b = Integer.parseInt(String.valueOf(s6));
+            s6 = s6.append(m.charAt(i));
+            System.out.println(s6);
+
 
         }
+        b = Integer.parseInt(String.valueOf(s6));
 
 
         System.out.println(Math.max(a, b));
