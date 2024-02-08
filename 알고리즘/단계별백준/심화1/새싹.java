2566f780d1875f25a694fefbf27e823aa56c1f04
Ernest45 <gkgkswns71@gmail.com>
2024-02-08 17:45:22 +0900

    새싹

---
+++ 알고리즘/단계별백준/심화1/새싹.java
@@ -0,0 +1,15 @@
+package 알고리즘.단계별백준.심화1;
+
+public class 새싹 {
+
+    public static void main(String[] args) {
+        String s = "         ,r'\"7\n" + 	// \", \n 이 제어문자다.
+                "r`-_   ,'  ,/\n" + 		// \n 이 제어문자다.
+                " \\. \". L_r'\n" + 		// \\, \", \n 이 제어문자다.
+                "   `~\\/\n" + 			// \\, \n 이 제어문자다.
+                "      |\n" + 			// \n 이 제어문자다.
+                "      |";
+
+        System.out.print(s);
+    }
+}
