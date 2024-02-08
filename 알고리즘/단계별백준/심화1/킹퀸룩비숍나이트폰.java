b6a977b4c6b5d33743ab36a8e710482c3983baf3
Ernest45 <gkgkswns71@gmail.com>
2024-02-08 18:37:24 +0900

    체스

---
+++ 알고리즘/단계별백준/심화1/킹퀸룩비숍나이트폰.java
@@ -0,0 +1,43 @@
+package 알고리즘.단계별백준.심화1;
+
+import java.util.Scanner;
+
+public class 킹퀸룩비숍나이트폰 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int k = sc.nextInt(); // 1 3
+        int q = sc.nextInt(); // 1
+        int l = sc.nextInt();// 2
+        int b = sc.nextInt();//2
+        int kn = sc.nextInt();//2
+        int p = sc.nextInt();//8
+
+        int kk = 1 - k;
+        int qq = 1 - q;
+        int ll = 2 - l;
+        int bb = 2 - b;
+        int knkn = 2 - kn;
+        int pp = 8 - p;
+
+
+
+        System.out.print(kk);
+        System.out.print(" ");
+        System.out.print(qq);
+        System.out.print(" ");
+        System.out.print(ll);
+        System.out.print(" ");
+        System.out.print(bb);
+        System.out.print(" ");
+        System.out.print(knkn);
+        System.out.print(" ");
+        System.out.print(pp);
+        }
+
+
+        }
+
+
