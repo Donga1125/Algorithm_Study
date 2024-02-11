438116d6fa26ad0bd6abe2e3f4bb71f10b6242de
Ernest45 <gkgkswns71@gmail.com>
2024-02-11 20:35:20 +0900

    그룹단어체커

---
+++ 알고리즘/단계별백준/심화1/그룹단어해결.java
@@ -0,0 +1,25 @@
+package 알고리즘.단계별백준.심화1;
+
+import java.util.Scanner;
+
+public class 그룹단어해결 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {
+            String str = sc.next();
+            boolean[] arr = new boolean[26];
+            boolean check = true;
+
+            for (int j = 0; j < str.length(); j++) {
+
+
+
+                }
+            }
+         }
+    }
+
--- 알고리즘/단계별백준/심화1/그룹단체체커ㅡ실패.java
+++ 알고리즘/단계별백준/심화1/그룹단체체커ㅡ실패.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.Stack;
 
 public class 그룹단체체커ㅡ실패 {
     // 한 알파벳이 들어오면 연속적으로만 들어오고 떨어지지만 않으면
@@ -29,15 +30,27 @@ public class 그룹단체체커ㅡ실패 {
 
 
 
-            for (int j = 0; j < str.length() - 1; j++) {
-                // 망한 원인 같을 경우에는 answer을 새로 만들어줌 폐기하자
+//            for (int j = 0; j < str.length() - 1; j++) {
+//
+//                if (str.charAt(j) == str.charAt(j + 1)) {
+//                    answer = answer + str.charAt(j);i++;
+//                }
+//
+//            }
 
-                if (str.charAt(j) == str.charAt(j + 1)) {
-                answer = answer + str.charAt(j);
+            Stack<Character> stack = new Stack<>();
+            stack.push(str.charAt(0));//일단 0번쨰 넣어주고 그 영번쨰에서 다음부터 하나씩 넣어주는데 다음 수가 같지 않으면 넣어주기
+            //즉 aaab면 0이랑 1,2까지 비교하고 같지 않은 3이 나오면 넣어줘서 ab로 만들어준다! 그럼 다음 peek은 2부터 꺼내게됨!
+
+            // 내가 만들려고 했던 거랑 비슷하지만 난 포문으로 만들고 다시 set으로 하는 과정에서 막혔다 (for문에서 연속된 수만 넣어줄 수 밖에없었다ㅠ)
+            for (int j = 1; j < str.length(); j++) {
+                if (stack.peek() != str.charAt(j)) {
+                    stack.push(str.charAt(j));
                 }
             }
-            answer = answer + str.charAt(str.length() - 1);
-            System.out.println(answer);
+            for (int j = 0; j < stack.size(); j++) {
+                answer += stack.get(j);
+            }
 
             Set<Character> set = new HashSet<>();
 
