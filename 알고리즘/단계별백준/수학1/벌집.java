ef8556f165f5ed05d91a7983eae7b3e3075841bb
Ernest45 <gkgkswns71@gmail.com>
2024-02-13 21:05:49 +0900

    벌집

--- 알고리즘/단계별백준/수학1/벌집.java
+++ 알고리즘/단계별백준/수학1/벌집.java
@@ -1,2 +1,26 @@
-package 알고리즘.단계별백준.수학1;public class 벌집 {
+package 알고리즘.단계별백준.수학1;
+
+import java.util.Scanner;
+
+public class 벌집 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int range = 2;
+        int count = 1;
+
+        if (n == 1) {
+            System.out.println(1);
+        } else {
+
+            while (range <= n) {
+                range = range + (count * 6);
+                count++;
+            }
+            System.out.println(count);
+        }
+
+
+    }
 }
