f6a803f4d97928184df9fbdde4b8d66a7e497e3c
Ernest45 <gkgkswns71@gmail.com>
2024-02-13 21:19:56 +0900

    분수찾기

--- 알고리즘/단계별백준/수학1/분수찾기.java
+++ 알고리즘/단계별백준/수학1/분수찾기.java
@@ -1,2 +1,36 @@
-package 알고리즘.단계별백준.수학1;public class 분수찾기 {
+package 알고리즘.단계별백준.수학1;
+
+import java.util.Scanner;
+
+public class 분수찾기 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int cross_count = 1, prev_count_sum = 0;
+
+        while (true) {
+
+            // 직전 대각선 누적합 + 해당 대각선 개수 이용한 범위 판별
+            if (n <= prev_count_sum + cross_count) {
+
+                if (cross_count % 2 == 1) {	// 대각선의 개수가 홀수라면
+                    // 분자가 큰 수부터 시작
+                    // 분자는 대각선상 내의 블럭 개수 - (X 번째 - 직전 대각선까지의 블럭 개수 - 1)
+                    // 분모는 X 번째 - 직전 대각선까지의 블럭 개수
+                    System.out.print((cross_count - (n - prev_count_sum - 1)) + "/" + (n - prev_count_sum));
+                    break;
+                }
+
+                else {	// 대각선상의 블럭의 개수가 짝수라면
+                    // 홀수일 때의 출력을 반대로
+                    System.out.print((n - prev_count_sum) + "/" + (cross_count - (n - prev_count_sum - 1)));
+                    break;
+                }
+
+            } else {
+                prev_count_sum += cross_count;
+                cross_count++;
+            }
+        }
+    }
 }
