30dc5639fe2ea82832fd05c48e14fe191606a92d
Ernest45 <gkgkswns71@gmail.com>
2024-02-13 19:07:48 +0900

    재귀

---
+++ 알고리즘/누구나자료구조알고리즘/재귀.java
@@ -0,0 +1,12 @@
+package 알고리즘.누구나자료구조알고리즘;
+
+public class 재귀 {
+    static public int sum(int[] arr) {
+        return arr[0] + sum(arr[);
+    }
+
+    public static void main(String[] args) {
+
+
+    }
+}
