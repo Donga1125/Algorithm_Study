e493ab25f91e7f557450e05b2bab29d9db0dbde8
Ernest45 <gkgkswns71@gmail.com>
2024-02-14 19:45:37 +0900

    달팽이는올라가고싶다

--- 알고리즘/단계별백준/수학1/달팽이는올라가고싶다.java
+++ 알고리즘/단계별백준/수학1/달팽이는올라가고싶다.java
@@ -1,31 +1,29 @@
 package 알고리즘.단계별백준.수학1;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class 달팽이는올라가고싶다 {
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int v = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int v = Integer.parseInt(st.nextToken());
 
-        int answer = 0;
 
-        int count = 0;
 
-        while (answer <= v) {
+        int answer = (v - b) / (a - b); // 반복문 없이 가능
 
-            answer = answer + a; // 0 + 2
-            if (answer == v) {
-                count++;
-                break;
-            }
-            answer = answer - b; // 2 -1
-            count++; //
+        if (((v - b)) % (a - b) != 0) {
+            answer++;
         }
 
-        System.out.println(count);
+
+        System.out.println(answer);
     }
 
 
