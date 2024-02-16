2327d6ce0b805cf3b2aa772f5d966e162ed9642a
Ernest45 <gkgkswns71@gmail.com>
2024-02-16 21:42:28 +0900

    수학은체육과목

---
+++ 알고리즘/단계별백준/기하직사각형삼각형/수학은체육과목입니다.java
@@ -0,0 +1,16 @@
+package 알고리즘.단계별백준.기하직사각형삼각형;
+
+import java.util.Scanner;
+
+public class 수학은체육과목입니다 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        long n = sc.nextLong();
+
+        n = n * 4;
+        System.out.println(n);
+    }
+}
