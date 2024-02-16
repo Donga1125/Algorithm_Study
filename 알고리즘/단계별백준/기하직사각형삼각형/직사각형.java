9f8b4b5cc6e4000524f18b416ede95449ebb4313
Ernest45 <gkgkswns71@gmail.com>
2024-02-16 20:27:52 +0900

    직사각형

---
+++ 알고리즘/단계별백준/기하직사각형삼각형/직사각형.java
@@ -0,0 +1,15 @@
+package 알고리즘.단계별백준.기하직사각형삼각형;
+
+import java.util.Scanner;
+
+public class 직사각형 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        System.out.println(n * m);
+    }
+}
