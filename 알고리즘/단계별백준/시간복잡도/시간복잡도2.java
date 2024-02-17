6f976022f9fb29c58b44d461c80032bebf1e749b
Ernest45 <gkgkswns71@gmail.com>
2024-02-17 20:44:36 +0900

    시간복잡도:

---
+++ 알고리즘/단계별백준/시간복잡도/시간복잡도2.java
@@ -0,0 +1,17 @@
+package 알고리즘.단계별백준.시간복잡도;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 시간복잡도2 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        System.out.println(n);
+        System.out.println(1);
+
+    }
+}
