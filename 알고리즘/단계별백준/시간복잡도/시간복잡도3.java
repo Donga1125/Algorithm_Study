260b98c5e0931608d1e4c78da31c4d18efc138d2
Ernest45 <gkgkswns71@gmail.com>
2024-02-17 20:47:47 +0900

    시간복잡도3

---
+++ 알고리즘/단계별백준/시간복잡도/시간복잡도3.java
@@ -0,0 +1,13 @@
+package 알고리즘.단계별백준.시간복잡도;
+
+import java.util.Scanner;
+
+public class 시간복잡도3 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println((long)n * n);
+        System.out.println(2);
+    }
+}
