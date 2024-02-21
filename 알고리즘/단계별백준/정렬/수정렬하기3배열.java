01610e7a5cdc614fc537e0d9b88caef6f00fc4a7
Ernest45 <readonme@naver.com>
2024-02-21 23:19:54 +0900

    수정렬하기ㅇ리스트말고배열

--- 알고리즘/단계별백준/정렬/수정렬하기3배열.java
+++ 알고리즘/단계별백준/정렬/수정렬하기3배열.java
@@ -3,11 +3,9 @@ package 알고리즘.단계별백준.정렬;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 
-public class 수정렬하기3카운팅소트 {
+public class 수정렬하기3배열 {
 
     //시간은 줄였는데 메모리를 줄여야 한다.. 어떠게 할까 다른 알고리즘들을 써야 할듯 ?
     // 시간 초과는 버퍼리더랑 스트링빌더로 해결하고 공간은 ArrayList를 쓰니 메모리 초과라서 int배열로 받아서 해결
