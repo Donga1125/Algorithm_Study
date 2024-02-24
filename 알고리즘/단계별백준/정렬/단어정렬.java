fc5dd10879c3c0854fbb30e1fd44a372881c97cd
Ernest45 <gkgkswns71@gmail.com>
2024-02-24 18:16:54 +0900

    단어정렬

---
+++ 알고리즘/단계별백준/정렬/단어정렬.java
@@ -0,0 +1,2 @@
+package 알고리즘.단계별백준.정렬;public class 단어정렬 {
+}
--- 알고리즘/단계별백준/정렬/좌표정렬하기2.java
+++ 알고리즘/단계별백준/정렬/좌표정렬하기2.java
@@ -1,2 +1,41 @@
-package 알고리즘.단계별백준.정렬;public class 좌표정렬하기2 {
+package 알고리즘.단계별백준.정렬;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class 좌표정렬하기2 {
+
+    //이번엔 반대로 y값 기준이고 x가 증가하게
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[][] arr = new int[n][2];
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr, (e1, e2) -> {
+            if (e1[1] == e2[1]) {
+                return e1[0] - e2[0];
+            } else
+                return e1[1] - e2[1];
+        });
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            sb.append(arr[i][0] + " "  + arr[i][1]).append('\n');
+
+        }
+
+        System.out.println(sb);
+
+    }
 }
