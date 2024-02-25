266905136725c155e839eb4f3646f61ac8311371
Ernest45 <readonme@naver.com>
2024-02-25 23:47:22 +0900

    a+b

---
+++ 알고리즘/단계별백준/수학1/a플마2.java
@@ -0,0 +1,14 @@
+package 알고리즘.단계별백준.수학1;
+
+import java.util.Scanner;
+
+public class a플마2 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println(a+b);
+    }
+}
