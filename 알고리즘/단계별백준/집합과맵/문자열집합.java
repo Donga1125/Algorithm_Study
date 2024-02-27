95b3de45ee513bc293fb8dce69bc552d73db621f
Ernest45 <gkgkswns71@gmail.com>
2024-02-27 21:06:52 +0900

    문자열집합

--- 알고리즘/단계별백준/집합과맵/문자열집합.java
+++ 알고리즘/단계별백준/집합과맵/문자열집합.java
@@ -1,2 +1,40 @@
-package 알고리즘.단계별백준.집합과맵;public class 문자열집합 {
+package 알고리즘.단계별백준.집합과맵;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.StringTokenizer;
+
+public class 문자열집합 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int count = 0;
+
+        HashSet<String> set = new HashSet<>();
+
+        String[] arrM = new String[m];
+
+        for (int i = 0; i < n; i++) {
+
+            set.add(br.readLine());
+
+
+        }
+
+        for (int i = 0; i < m; i++) {
+            arrM[i] = br.readLine();
+            if(set.contains(arrM[i])){
+                count++;
+            }
+        }
+
+        System.out.println(count);
+
+    }
 }
---
+++ 알고리즘/단계별백준/집합과맵/출입카드.java
@@ -0,0 +1,2 @@
+package 알고리즘.단계별백준.집합과맵;public class 출입카드 {
+}
