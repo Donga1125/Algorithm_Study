5d570a54958f56ac63007f5ae1651d89d1feffb1
Ernest45 <gkgkswns71@gmail.com>
2024-02-28 20:10:56 +0900

    최소공배수

--- 기초2/Day30/임한준/일이삼더하기.java
+++ 기초2/Day30/임한준/일이삼더하기.java
@@ -36,11 +36,13 @@ public class 일이삼더하기 {
         dp[2]=2;
         dp[3]=4;
 
-        for(int j=4;j<=10;j++) { // 4부터 반복
-            dp[j] = dp[j-3] + dp[j-2] + dp[j-1]; // 점화식
+        for (int j = 4; j <= 10; j++) { // 4부터 반복
+
+            dp[j] = dp[j - 3] + dp[j - 2] + dp[j - 1]; // 점화식
         }
 
-        for(int i=0;i<t;i++) {
+        for (int i = 0; i < t; i++) {
+
             int n = sc.nextInt();
 
             System.out.println(dp[n]);
--- 알고리즘/단계별백준/약수배수소수2/최소공배수.java
+++ 알고리즘/단계별백준/약수배수소수2/최소공배수.java
@@ -1,4 +1,52 @@
 package 알고리즘.단계별백준.약수배수소수2;
 
-public class 약수배수소수2 {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 최소공배수 {
+    // 더 작은 수를 계속 곱해서 max 밸류까지 도착했을 때 없으면
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+
+
+        for (int i = 0; i < t; i++) {
+
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            int count1 = a;
+            int count2 = b;
+
+
+
+            while (true) {
+
+                if(count1 < count2) {
+
+                count1 = count1 + a;
+
+                } else if (count1 == count2) {
+
+                    sb.append(count1).append('\n');
+
+                    break;
+
+                } else
+                    count2 = count2 + b;
+
+            }
+        }
+
+        System.out.println(sb);
+
+    }
 }
