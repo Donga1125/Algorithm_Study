b8193c5df87862e4395661533e0e50e0adb5178f
Ernest45 <gkgkswns71@gmail.com>
2024-03-02 18:05:17 +0900

    가로수 실패이유

---
+++ 알고리즘/단계별백준/약수배수소수2/가로수실패.java
@@ -0,0 +1,55 @@
+package 알고리즘.단계별백준.약수배수소수2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class 가로수실패 {
+    // 주어진 수 중에 사이에 있는 가장 작은 걸 찾아야 하네 ( 증가도 만족 해야하는데 그건 고려 안하고 사이 수중 작은 것만 찾으면 될듯
+    // 중간에만 심을 수 있다
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+
+            arr[i] = Integer.parseInt(br.readLine());
+
+
+
+        }
+        int end = arr[n - 1];
+        int start = arr[0];
+        int minInterval = Integer.MAX_VALUE;
+        for (int i = 1; i < n; i++) {
+            minInterval = (Math.min(minInterval, arr[i] - arr[i - 1])); // 배열 중 가장 작은 수를 찾음
+        }
+
+
+        // 가로수를 심을 위치를 계산하고 리스트에 추가
+
+        while (true) {
+            list.add(start);
+            start = start + minInterval;
+
+
+
+            if (start == end) {
+                list.add(start);
+                break;
+            }
+        }
+        for (int t : list) {
+            System.out.println("t = " + t);
+
+        }
+
+        System.out.println(list.size() - arr.length);
+    }
+}
+
