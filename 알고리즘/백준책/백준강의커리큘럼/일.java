fe88db76950804d0c0f4759a725ce21dadc96685
Ernest45 <gkgkswns71@gmail.com>
2024-03-05 19:45:44 +0900

    백준커리큘럼 일

--- 알고리즘/백준책/백준강의커리큘럼/일.java
+++ 알고리즘/백준책/백준강의커리큘럼/일.java
@@ -1,2 +1,21 @@
-package 알고리즘.백준책.백준강의커리큘럼;public class 일 {
+package 알고리즘.백준책.백준강의커리큘럼;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 일 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        while (true) {
+
+            int n = Integer.parseInt(br.readLine());
+
+
+
+        }
+            
+    }
 }
