205834eacf96ecfdafffb885b73cd5adfb1fa470
Ernest45 <gkgkswns71@gmail.com>
2024-03-12 21:44:50 +0900

    피보나치수5

--- 알고리즘/단계별백준2/재귀/파보나치수5.java
+++ 알고리즘/단계별백준2/재귀/파보나치수5.java
@@ -1,2 +1,28 @@
-package 알고리즘.단계별백준2.재귀;public class 파보나치수5 {
+package 알고리즘.단계별백준2.재귀;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 파보나치수5 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        System.out.println(fibo(t));
+
+    }
+
+    public static long fibo(int t) {
+
+        if (t == 0) {
+            return 0;
+        }
+        if (t == 1 || t == 2) {
+            return 1;
+        }
+
+        return fibo(t - 1) + fibo(t - 2);
+
+    }
 }
