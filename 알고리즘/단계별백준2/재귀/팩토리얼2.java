a068ecbaa2f3f5d02586a7114a6c50e887a0444b
Ernest45 <gkgkswns71@gmail.com>
2024-03-12 21:44:33 +0900

    팩토리얼2 재귀

--- 알고리즘/단계별백준2/재귀/팩토리얼2.java
+++ 알고리즘/단계별백준2/재귀/팩토리얼2.java
@@ -1,4 +1,26 @@
 package 알고리즘.단계별백준2.재귀;
 
-public class 팩토리얼 {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 팩토리얼2 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Long n = Long.parseLong(br.readLine());
+        System.out.println(factorial(n));
+
+    }
+
+    public static long factorial(long a) {
+
+        if (a == 1 || a == 0) {
+
+            return 1;
+        } else
+            return a * factorial(a - 1);
+
+
+    }
 }
