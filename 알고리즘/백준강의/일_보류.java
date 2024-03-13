f6ba359733098e8b7dab68462e8f2a65dd99d228
Ernest45 <gkgkswns71@gmail.com>
2024-03-13 19:44:11 +0900

    일 보류

---
+++ 알고리즘/백준강의/일_보류.java
@@ -0,0 +1,24 @@
+package 알고리즘.백준강의;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 일_보류 {
+    //4375 /
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+        while (true) {
+
+
+            int t  = Integer.parseInt(br.readLine());
+
+        }
+
+
+
+    }
+}
