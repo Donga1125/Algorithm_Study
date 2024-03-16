afa8ca4cba6a42b42f7d2b05040424631d2da397
Ernest45 <readonme@naver.com>
2024-03-16 23:22:16 +0900

    백트레킹 n과m4

---
+++ 알고리즘/단계별백준2/백트레킹/n과m4.java
@@ -0,0 +1,34 @@
+package 알고리즘.단계별백준2.백트레킹;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class n과m4 {
+
+    private static int[] arr;
+    private static int n, m;
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        backTracking();
+
+    }
+
+    private static void backTracking(int depth) {
+
+
+        if (depth == m) {
+
+        }
+
+    }
+
+
