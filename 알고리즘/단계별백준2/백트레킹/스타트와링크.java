5f6f2112452a11bc02773b0e3293a2e092fc5cc7
Ernest45 <readonme@naver.com>
2024-03-17 23:27:52 +0900

    스타트와링크

--- 알고리즘/단계별백준2/백트레킹/스타트와링크.java
+++ 알고리즘/단계별백준2/백트레킹/스타트와링크.java
@@ -1,8 +1,11 @@
 package 알고리즘.단계별백준2.백트레킹;
 
+import java.io.*;
+
 public class 스타트와링크 {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
     }
 }
