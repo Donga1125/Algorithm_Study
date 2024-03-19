403d39e4f8658f6790d89c32accc0088a16ce44c
Ernest45 <gkgkswns71@gmail.com>
2024-03-19 21:16:16 +0900

    스타트와링크이해x

--- 알고리즘/단계별백준2/백트레킹/스타트와링크.java
+++ 알고리즘/단계별백준2/백트레킹/스타트와링크.java
@@ -1,11 +1,31 @@
 package 알고리즘.단계별백준2.백트레킹;
 
 import java.io.*;
+import java.util.StringTokenizer;
 
 public class 스타트와링크 {
 
+    // n/2 했을 때 경우의 수 저장하고, ( ㅏ짝수 수 들어옴
+    private static int[][] board;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+
+        board = new int[t][t];
+
+        for (int i = 0; i < t; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < t; j++) {
+                board[i][j] = Integer.parseInt(st.nextToken());
+            }
+            
+        }
+
+
+
+
+
 
     }
 }
