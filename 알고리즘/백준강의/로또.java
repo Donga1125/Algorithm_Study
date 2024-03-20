a0e89da15b54f274cf1f86dd48bd92684c264321
Ernest45 <gkgkswns71@gmail.com>
2024-03-20 22:48:54 +0900

    로또추가

--- 알고리즘/백준강의/로또.java
+++ 알고리즘/백준강의/로또.java
@@ -3,12 +3,35 @@ package 알고리즘.백준강의;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class 로또 {
     //6603
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine());
 
+
+        while (true) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+
+            if (n == 0) {
+                return;
+            }
+
+            for (int i = 0; i <n; i++) {=
+
+            }
+
+
+        }
+
+        }
+        int t = Integer.parseInt(st.nextToken());
+
+        for (int i = 0; i < t; i++) {
+            
+
+        }
     }
 }
