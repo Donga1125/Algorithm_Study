69c73ce6ed176ec7032f1f639636fe8265b08039
Ernest45 <gkgkswns71@gmail.com>
2024-03-22 22:06:08 +0900

    dfs완탐 퇴사

--- 알고리즘/백준강의/퇴사dfs2.java
+++ 알고리즘/백준강의/퇴사dfs2.java
@@ -12,7 +12,7 @@ public class 퇴사dfs2 {
 
     //n이 15라서 완탐가능 dfs만 돌려도 됨
 
-   private static int[][] dp;
+   private static int[][] arr;
     private static int n;
     private static int max = Integer.MIN_VALUE;
 
@@ -22,14 +22,14 @@ public class 퇴사dfs2 {
         n = Integer.parseInt(br.readLine());
 
 
-        dp = new int[n][2];
+        arr = new int[n][2];
 
 
 
         for (int i = 0; i < n; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            dp[i][0] = Integer.parseInt(st.nextToken()); // 시간
-            dp[i][1] = Integer.parseInt(st.nextToken()); // 금액
+            arr[i][0] = Integer.parseInt(st.nextToken()); // 시간
+            arr[i][1] = Integer.parseInt(st.nextToken()); // 금액
 
         }
 
@@ -45,14 +45,14 @@ public class 퇴사dfs2 {
     private static void dfs(int day, int benefit) {
 
         max = Math.max(benefit, max); // 마지막 날에 우리가 계산하는 게 아니라 돌 때 마다 max값을 갱신해줘야한다
-        // 막날이 꼭 최대가 아니라서
+        // 막날이 꼭 최대가 아니라ㅜ
 
         if (day >= n) {
             return;
         }
 
-        if (day + dp[day][0] <= n) { //상담이 가능하다면 ?
-            dfs(day + dp[day][0], benefit + dp[day][1]);
+        if (day + arr[day][0] <= n) { //상담이 가능하다면 ?
+            dfs(day + arr[day][0], benefit + arr[day][1]);
 
         } else { // 상담이 불가능하다면 ?
             dfs(day + 1, benefit);
