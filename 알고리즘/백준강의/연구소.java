176c5bfa6ee6d992fcd8bbc9d32bfd9e22e4918f
Ernest45 <gkgkswns71@gmail.com>
2024-03-23 23:37:47 +0900

    연구소

--- 알고리즘/백준강의/연구소.java
+++ 알고리즘/백준강의/연구소.java
@@ -1,2 +1,14 @@
-package 알고리즘.백준강의;public class 연구소 {
+package 알고리즘.백준강의;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 연구소 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int f = Integer.parseInt(br.readLine());
+
+    }
 }
