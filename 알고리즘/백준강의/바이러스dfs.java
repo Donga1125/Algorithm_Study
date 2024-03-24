37a596d86a630133362038c616895653fe14f77b
Ernest45 <gkgkswns71@gmail.com>
2024-03-24 23:30:42 +0900

    바이러스dfs

--- 알고리즘/백준강의/바이러스dfs.java
+++ 알고리즘/백준강의/바이러스dfs.java
@@ -1,2 +1,14 @@
-package 알고리즘.백준강의;public class 바이러스dfs {
+package 알고리즘.백준강의;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 바이러스dfs {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int c = Integer.parseInt(br.readLine());
+
+    }
 }
