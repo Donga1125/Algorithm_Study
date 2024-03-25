1c2be8d322ffccd97d8ca3ee453b0ceb37093eb0
Ernest45 <gkgkswns71@gmail.com>
2024-03-25 20:18:55 +0900

    피보나치수2

--- 알고리즘/백준강의/피보나치수2.java
+++ 알고리즘/백준강의/피보나치수2.java
@@ -1,2 +1,44 @@
-package 알고리즘.백준강의;public class 피보나치수2 {
+package 알고리즘.백준강의;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class 피보나치수2 {
+
+    static long[] arr;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        arr = new long[n + 1];
+        Arrays.fill(arr, -1);
+
+        fib(n);
+        System.out.println(fib(n));
+
+
+
+
+
+
+    }
+
+    private static long fib(int n) {
+
+        arr[0] = 0;
+
+        arr[1] = 1;
+
+        if (arr[n] == -1) {
+            arr[n] = fib(n-1) + fib(n-2);
+        }
+
+        return arr[n];
+
+    }
+
+
 }
