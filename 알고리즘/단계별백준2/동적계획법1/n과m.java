6e4b7910ef169b3e12047b5416cf78bb10d93566
Ernest45 <gkgkswns71@gmail.com>
2024-03-26 23:23:26 +0900

    n과m 가장긴수열

---
+++ 알고리즘/단계별백준2/동적계획법1/n과m.java
@@ -0,0 +1,14 @@
+package 알고리즘.단계별백준2.동적계획법1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class n과m {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+
+    }
+}
