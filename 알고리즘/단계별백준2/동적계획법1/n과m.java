5cb628f389f0d7d355fb4ca00af5761d02c97b7d
Ernest45 <gkgkswns71@gmail.com>
2024-03-26 23:23:54 +0900

    n과m5 가장긴수열

--- 알고리즘/단계별백준2/동적계획법1/n과m.java
+++ 알고리즘/단계별백준2/동적계획법1/n과m.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class n과m {
+    //가장 긴 수열 만들어야 하는데 순환 반복이 어렵다
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
