de2ca42786c03f805b1da526ea97ff50cde7ed8d
Ernest45 <gkgkswns71@gmail.com>
2024-03-27 19:14:18 +0900

    dp배열이용 일로만들기

--- 알고리즘/단계별백준2/동적계획법1/일로만들기dp배열.java
+++ 알고리즘/단계별백준2/동적계획법1/일로만들기dp배열.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class 일로만들기 {
+public class 일로만들기dp배열 {
 
     //1463
     private static int n;
