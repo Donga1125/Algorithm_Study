f7a9b2af82ec4f3ea059877df05b26c5527cd600
Ernest45 <gkgkswns71@gmail.com>
2024-03-30 23:41:49 +0900

    재귀

--- 알고리즘/백준책/백준강의커리큘럼/브루트포스/재귀/계단오르기.java
+++ 알고리즘/백준책/백준강의커리큘럼/브루트포스/재귀/계단오르기.java
@@ -3,12 +3,30 @@ package 알고리즘.백준책.백준강의커리큘럼.브루트포스.재귀;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class 계단오르기 {
+    //2579
+    static Integer dp[];
+    static int arr[];
+
+    // 점화식은 f(n-1) + f(n-2)
     
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(br.readLine());
-            
+
+        for (int i = 0; i < t; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            arr[i] = Integer.parseInt(st.nextToken());
+
+        }
+    }
+
+    private static void dfs(int n) {
+
+        if (dp[n] == null) {
+            dp[n] == Math.max(dfs(n - 2), dfs(n - 3) + arr[n - 1]) + arr[n];
+        }
     }
 }
