102fd18f8436c329928f3d2d707ff808242665af
Ernest45 <gkgkswns71@gmail.com>
2024-03-30 23:19:25 +0900

    계단오르기

---
+++ 알고리즘/백준책/백준강의커리큘럼/브루트포스/재귀/계단오르기.java
@@ -0,0 +1,14 @@
+package 알고리즘.백준책.백준강의커리큘럼.브루트포스.재귀;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 계단오르기 {
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+            
+    }
+}
