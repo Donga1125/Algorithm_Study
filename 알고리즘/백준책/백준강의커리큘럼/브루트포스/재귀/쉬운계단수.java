d64d5e4169ae288eb474204c9666fe09cd27e1b2
Ernest45 <gkgkswns71@gmail.com>
2024-03-31 23:51:17 +0900

    쉬운계단 풀이

--- 알고리즘/백준책/백준강의커리큘럼/브루트포스/재귀/쉬운계단수.java
+++ 알고리즘/백준책/백준강의커리큘럼/브루트포스/재귀/쉬운계단수.java
@@ -1,4 +1,15 @@
 package 알고리즘.백준책.백준강의커리큘럼.브루트포스.재귀;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class 쉬운계단수 {
+    //10844
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+            
+    }
 }
