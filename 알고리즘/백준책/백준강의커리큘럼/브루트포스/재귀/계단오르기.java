3ca5ac77057f8f230fa854e4140727eca17baf96
Ernest45 <gkgkswns71@gmail.com>
2024-04-01 20:23:58 +0900

    계단 오르기 최종

--- 알고리즘/백준책/백준강의커리큘럼/브루트포스/재귀/계단오르기.java
+++ 알고리즘/백준책/백준강의커리큘럼/브루트포스/재귀/계단오르기.java
@@ -16,11 +16,22 @@ public class 계단오르기 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(br.readLine());
 
-        for (int i = 0; i < t; i++) {
+        arr = new int[t +1];
+        dp = new Integer[t +1];
+
+        for (int i = 1; i <= t; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             arr[i] = Integer.parseInt(st.nextToken());
 
         }
+        dp[0] = arr[0]; // null 값 초기화 //
+        dp[1] = arr[1];
+
+        if (t >= 2) {
+            dp[2] = arr[1] + arr[2];
+        }
+        System.out.println(dfs(t));
+
     }
 
     private static int dfs(int n) {
