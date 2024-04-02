962f31cde6e07056c1241c7d33476c07b7e80c0a
Ernest45 <gkgkswns71@gmail.com>
2024-04-02 21:07:49 +0900

    가장긴감소하는수열

--- 알고리즘/백준책/백준강의커리큘럼/브루트포스/재귀/가장긴감소하는수열.java
+++ 알고리즘/백준책/백준강의커리큘럼/브루트포스/재귀/가장긴감소하는수열.java
@@ -31,19 +31,27 @@ public class 가장긴감소하는수열 {
 
         int max = 1;
 
-        for (int i = n - 1; i < 0; i--) {
-            for (int j = n - 1; j < i; j--) {
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (arr[i] < arr[j] && dp[i] <= dp[j]) {
 
-                if (arr[i] > arr[j]) {
-                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                    dp[i] = dp[j] + 1;
+                } else if (arr[i] == arr[j]) {
+                    dp[i] = dp[j];
+                }
 
                 }
             }
 
+        for (int i = 0; i < n; i++) {
             max = Math.max(dp[i], max);
         }
 
         System.out.println(max);
+
+
+
+
             
     }
 }
