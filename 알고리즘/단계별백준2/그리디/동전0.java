3bcd9d177bbdb61678bc6a4f52afcaa55083bd12
Ernest45 <gkgkswns71@gmail.com>
2024-04-03 22:45:20 +0900

    그리디 동전

---
+++ 알고리즘/단계별백준2/그리디/동전0.java
@@ -0,0 +1,17 @@
+package 알고리즘.단계별백준2.그리디;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 동전0 {
+
+    //11047
+
+    //그리디 동전
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+            
+    }
+}
