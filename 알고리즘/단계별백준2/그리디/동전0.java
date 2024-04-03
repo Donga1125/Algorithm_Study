13dada8f6611443c05de0cb0e93ae29449bdba27
Ernest45 <gkgkswns71@gmail.com>
2024-04-03 22:55:28 +0900

    그리디 동전 ㅣㄹ패

--- 알고리즘/단계별백준2/그리디/동전0.java
+++ 알고리즘/단계별백준2/그리디/동전0.java
@@ -3,6 +3,7 @@ package 알고리즘.단계별백준2.그리디;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class 동전0 {
 
@@ -11,7 +12,30 @@ public class 동전0 {
     //그리디 동전
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int t = Integer.parseInt(st.nextToken());
+        int price = Integer.parseInt(st.nextToken());
+        int count = 0;
+        
+        int arr[] = new int[t];
+
+        for (int i = 0; i < t; i++) {
+            st = new StringTokenizer(br.readLine());
+            arr[i] = Integer.parseInt(st.nextToken());
+
+        }
+
+        for (int i = arr.length - 1; i > 0; i--) {
+            if (arr[i] <= price) {
+                price = price - arr[i];
+                count++;
+
+
+            }
+        }
+
+//
+        System.out.println(count);
             
     }
 }
