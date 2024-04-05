f9b4a7b5234d7969c862c457eaf57681af4cd710
Ernest45 <gkgkswns71@gmail.com>
2024-04-05 21:14:00 +0900

    재귀실험5

--- 알고리즘/백준책/백준강의커리큘럼/브루트포스/재귀/블로그재귀실험5.java
+++ 알고리즘/백준책/백준강의커리큘럼/브루트포스/재귀/블로그재귀실험5.java
@@ -1,19 +1,20 @@
 package 알고리즘.백준책.백준강의커리큘럼.브루트포스.재귀;
 
-public class 블로그재귀실험4 {
+public class 블로그재귀실험5 {
+    static int n = 3;
 
 
 
     public static void main(String[] args) {
-        System.out.println(factorial(4));
+        System.out.println(reReturn(0));
 
     }
 
-    private static int factorial(int k) {
-        if (k == 1) {
+    private static int reReturn(int k) {
+        if (k == n) {
             return 1;
 
         }
-        return k * factorial(k - 1);
+        return reReturn(k + 1) + reReturn(k + 1);
     }
 }
