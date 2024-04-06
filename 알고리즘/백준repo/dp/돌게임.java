d10f9c7500b440339f4ad1f383e22e2f27cddac5
Ernest45 <gkgkswns71@gmail.com>
2024-04-06 23:26:00 +0900

    돌게임 9655

---
+++ 알고리즘/백준repo/dp/돌게임.java
@@ -0,0 +1,15 @@
+package 알고리즘.백준repo.dp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 돌게임 {
+    //9655
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+
+    }
+}
