965f45cadbb60a0957190188a3ce20bd6b081177
Ernest45 <gkgkswns71@gmail.com>
2024-04-06 23:28:35 +0900

    Four Squares

---
+++ 알고리즘/백준repo/dp/fourSquares.java
@@ -0,0 +1,15 @@
+package 알고리즘.백준repo.dp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class fourSquares {
+
+    //17626
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+    }
+}
