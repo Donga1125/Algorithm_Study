c5bc38526b4809173bad33fc34fef3731fdadb53
Ernest45 <gkgkswns71@gmail.com>
2024-04-10 22:42:49 +0900

    2xn타일링 2

---
+++ 알고리즘/백준repo/dp/투xn타일링투.java
@@ -0,0 +1,16 @@
+package 알고리즘.백준repo.dp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 투xn타일링투 {
+
+    //11727
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+
+    }
+}
