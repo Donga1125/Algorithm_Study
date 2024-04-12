0c51a6cf6cfb6db62851ee8527808a378de947d8
Ernest45 <gkgkswns71@gmail.com>
2024-04-12 21:11:30 +0900

    FourSquares 제대로된 해석

--- 알고리즘/백준repo/dp/fourSquares정답.java
+++ 알고리즘/백준repo/dp/fourSquares정답.java
@@ -5,35 +5,41 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 
-public class fourSquares {
+public class fourSquares정답 {
 
     //17626
     //최소한의 제곱근을 만들어라
 
     private static int[] dp;
+    private static int min = 0;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
 
         dp = new int[n + 1];
-        Arrays.fill(dp,50001);
-
-        System.out.println(dp(n));
-
-    }
-
-    private static int dp(int n) {
-
         dp[1] = 1;
-        dp[2] = 2;
-        dp[3] = 3;
 
-        if (dp[n] > 50001) {
+        for (int i = 2; i <= n; i++) {
+            min = Integer.MAX_VALUE; // min 값을 초기화
+            for (int j = 1; j * j <= i; j++) { // 제곱근 확인
+                int temp = i - j * j; // 제곱근 빼고 남은 수랑 비교하기 위해
+                min = Math.min(min, dp[temp]); // 작은 수를 저장해주고
 
-            return dp[n] = Math.max(dp(n / 2), dp(n - 1) + 1);
+            }
 
+            //dp[i]가 저장되는 과정은 i보다 작은 제곱근을 구해서  예로 i가 4라면 제곱근은 1과 2 두개가 있는데
+            // 그보다 작은 수
+
+            dp[i] = min + 1;
         }
 
-        return dp[n];
+        System.out.println(dp[n]);
+
+
+
+
     }
+
+
+
 }
