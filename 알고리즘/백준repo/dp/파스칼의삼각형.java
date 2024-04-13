8aedce5d706167bb97c9f55bc8e6d02aa0640f21
Ernest45 <gkgkswns71@gmail.com>
2024-04-13 23:06:34 +0900

    파스칼의 삼각형

---
+++ 알고리즘/백준repo/dp/파스칼의삼각형.java
@@ -0,0 +1,19 @@
+package 알고리즘.백준repo.dp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 파스칼의삼각형 {
+    //15489
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int v = Integer.parseInt(st.nextToken());
+
+    }
+}
