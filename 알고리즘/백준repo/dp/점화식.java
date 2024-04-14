2d5d5015a0b51d59dbeb382044a78e6a9cc2d67f
Ernest45 <gkgkswns71@gmail.com>
2024-04-14 22:29:15 +0900

    점화식 추가설명

--- 알고리즘/백준repo/dp/점화식.java
+++ 알고리즘/백준repo/dp/점화식.java
@@ -13,21 +13,18 @@ public class 점화식 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(br.readLine());
 
-        dp = new long[t + 1];
+        dp = new long[36];
 
         dp[0] = 1;
         dp[1] = 1;
 
 
-        for (int i = 2; i <= t; i++) {
+        for (int i = 2; i < 36; i++) {
             for (int j = 0; j < i; j++) {
                 dp[i] = dp[i] + (dp[j] * dp[i - 1 - j]);
-            }
+            } // 처음에 dp[i]의 값이 어디서 가져오지 했는데 기본 0의 값부터 시작햇 ㅓ계속 더해 가는 걸 놓쳤다..
 
-
-    }
+        }
         System.out.println(dp[t]);
 
-
-    }
-}
+    }}
