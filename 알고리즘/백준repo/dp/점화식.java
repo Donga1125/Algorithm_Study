8e5365d81456640ad787705aef01ebf1cada2c64
Ernest45 <gkgkswns71@gmail.com>
2024-04-14 22:16:06 +0900

    점화식

--- 알고리즘/백준repo/dp/점화식.java
+++ 알고리즘/백준repo/dp/점화식.java
@@ -6,28 +6,28 @@ import java.io.InputStreamReader;
 
 public class 점화식 {
 
-    private static int[] dp;
+    private static long[] dp;
     //13699
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(br.readLine());
 
-        dp = new int[t + 1];
-
-        System.out.println(dp(t));
-
-    }
-
-    private static int dp(int t) {
+        dp = new long[t + 1];
 
         dp[0] = 1;
+        dp[1] = 1;
 
 
-        for (int i = 0; i < t; i++) {
-            dp[t] = dp(i) * dp(t - 1);
-        }
+        for (int i = 2; i <= t; i++) {
+            for (int j = 0; j < i; j++) {
+                dp[i] = dp[i] + (dp[j] * dp[i - 1 - j]);
+            }
 
-        return dp[t];
+
+    }
+        System.out.println(dp[t]);
+
+
     }
 }
