94a8f311586faa3c4b01be430507ecb659878f21
Ernest45 <gkgkswns71@gmail.com>
2024-04-15 23:31:01 +0900

    연속부분최대곱

---
+++ 알고리즘/백준repo/dp/연속부분최대곱.java
@@ -0,0 +1,14 @@
+package 알고리즘.백준repo.dp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 연속부분최대곱 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+
+    }
+}
