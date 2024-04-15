04d2bc81c9bed6d20ee4849d8a54883ea69959f7
Ernest45 <gkgkswns71@gmail.com>
2024-04-15 23:34:51 +0900

    맥시멈어레이

--- 알고리즘/백준repo/dp/MaximumSubarray.java
+++ 알고리즘/백준repo/dp/MaximumSubarray.java
@@ -3,12 +3,29 @@ package 알고리즘.백준repo.dp;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class MaximumSubarray {
 
+    static int[] arr;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int anInt = Integer.parseInt(br.readLine());
+        int t = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < t; i++) {
+            int n = Integer.parseInt(br.readLine());
+
+            arr = new int[n];
+
+            for (int j = 0; j < n; j++) {
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                arr[j] = Integer.parseInt(st.nextToken());
+
+
+
+            }
+        }
 
     }
 }
