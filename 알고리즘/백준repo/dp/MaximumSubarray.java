45cee58a90534ccb5727656411acea8114270ecb
Ernest45 <gkgkswns71@gmail.com>
2024-04-16 21:30:36 +0900

    맥시멈서브어레이 데이터 유형 틀림

--- 알고리즘/백준repo/dp/MaximumSubarray.java
+++ 알고리즘/백준repo/dp/MaximumSubarray.java
@@ -6,26 +6,40 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class MaximumSubarray {
+    //10211
+    // 최대 부분배열을 구하는 문제
+    // 주어진 arr[i]와 dp[i-1] + drr[i]의 수를 비교해서 더 큰 값을 저장
+    // 1000 1000 이 가능
 
     static int[] arr;
+    static int[] dp;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(br.readLine());
 
+        StringBuilder sb = new StringBuilder();
+
         for (int i = 0; i < t; i++) {
             int n = Integer.parseInt(br.readLine());
 
-            arr = new int[n];
+            arr = new int[n+1];
+            dp = new int[n+1];
+            int max = 0;
+            StringTokenizer st = new StringTokenizer(br.readLine());
 
-            for (int j = 0; j < n; j++) {
-                StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= n; j++) {
+
                 arr[j] = Integer.parseInt(st.nextToken());
-
-
+                dp[j] = Math.max(arr[j], dp[j - 1] + arr[j]);
+                max = Math.max(max, dp[j]);
 
             }
+            sb.append(max).append('\n');
+
         }
+
+        System.out.println(sb);
 
     }
 }
