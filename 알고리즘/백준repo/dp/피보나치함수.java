7a5b18f9a6bca3ff07ca50beb073c9ecfd846656
Ernest45 <gkgkswns71@gmail.com>
2024-04-18 19:51:12 +0900

    피보나치함수 시간초과

---
+++ 알고리즘/백준repo/dp/피보나치함수.java
@@ -0,0 +1,61 @@
+package 알고리즘.백준repo.dp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 피보나치함수 {
+
+    //1003 0 or 1 얼마나 많이 출력 하는 지 배열 형태로
+
+    private static Integer[] dp;
+    private static int[] or;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t= Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < t; i++) {
+            int n = Integer.parseInt(br.readLine());
+
+            dp = new Integer[41];
+
+            or = new int[2];
+
+            fibo(n);
+
+            sb.append(or[0]).append(" ").append(or[1]).append('\n');
+
+
+        }
+
+        System.out.println(sb);
+
+
+    }
+
+    private static int fibo(int n) {
+
+        if (n == 0) {
+            or[0]++;
+        }
+        if (n == 1) {
+            or[1]++;
+        }
+
+        dp[0] = 0;
+        dp[1] = 1;
+
+        if (dp[n] == null) {
+
+            return fibo(n - 1) + fibo(n - 2);
+
+        }
+
+        return dp[n];
+
+    }
+}
