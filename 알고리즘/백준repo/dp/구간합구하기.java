ce19b5f35eac8aa0fd76eeecaae974fa18d03acc
Ernest45 <gkgkswns71@gmail.com>
2024-04-20 23:41:03 +0900

    구간합구하기

---
+++ 알고리즘/백준repo/dp/구간합구하기.java
@@ -0,0 +1,16 @@
+package 알고리즘.백준repo.dp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 구간합구하기 {
+
+    //11660
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int d = Integer.parseInt(br.readLine());
+
+    }
+}
