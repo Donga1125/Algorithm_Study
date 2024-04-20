3e325d7079fbeaf30ef4a0687bb805280d6b5c3c
Ernest45 <gkgkswns71@gmail.com>
2024-04-20 23:52:35 +0900

    구간합구하기5

--- 알고리즘/백준repo/dp/구간합구하기5.java
+++ 알고리즘/백준repo/dp/구간합구하기5.java
@@ -3,14 +3,19 @@ package 알고리즘.백준repo.dp;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-public class 구간합구하기 {
+public class 구간합구하기5 {
 
     //11660
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int d = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int d = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+
 
     }
 }
