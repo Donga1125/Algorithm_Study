ec125f601ab751d912904578c7ddbb4f8580a08a
Ernest45 <gkgkswns71@gmail.com>
2024-04-20 23:40:27 +0900

    쉬운계딴수

---
+++ 알고리즘/백준repo/dp/쉬운계단수.java
@@ -0,0 +1,16 @@
+package 알고리즘.백준repo.dp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 쉬운계단수 {
+
+    //10844
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+
+    }
+}
