db9d2cb6fa3cf47b45da83dd94345f5def6348c8
Ernest45 <gkgkswns71@gmail.com>
2024-04-23 22:05:59 +0900

    구간합5

--- 알고리즘/백준repo/dp/구간합구하기5.java
+++ 알고리즘/백준repo/dp/구간합구하기5.java
@@ -11,6 +11,9 @@ public class 구간합구하기5 {
     //2,2 -> 3,4 구하라고 하면 22 23 24 32 33 34구하기다 21은 포함안됨
     static int[][] arr;
     static int[][] wanted;
+    static int[][] from;
+    static int[][] to;
+    static long[][] dp;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -20,6 +23,11 @@ public class 구간합구하기5 {
         arr = new int[n][n];
 
         wanted = new int[m][4];
+        dp = new long[n][n];
+        from = new int[m][2];
+        to = new int[m][2];
+
+
 
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
@@ -31,15 +39,33 @@ public class 구간합구하기5 {
             
         }
 
-        for (int i = 0; i <m; i++) {
+        for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
+            int x = 0;
+            int y = 0;
             for (int j = 0; j < 4; j++) {
-                wanted[i][j] = Integer.parseInt(st.nextToken());
-                System.out.print(wanted[i][j] + " ");
+//                wanted[i][j] = Integer.parseInt(st.nextToken());
+                x = wanted[i][j];
+                y = wanted[i][j];
+
+                System.out.println(x + " " + y);
+
+
             }
-            System.out.println();
+            for (int j = 0; j < 2; j++) {
+                from[i][j] = Integer.parseInt(st.nextToken());
+
+            }
+            for (int j = 0; j < 2; j++) {
+                to[i][j] = Integer.parseInt(st.nextToken());
+
+            }
+
+
+
             
         }
+
 
 
 
