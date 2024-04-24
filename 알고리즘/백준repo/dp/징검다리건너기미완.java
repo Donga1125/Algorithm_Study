d1c8582cff9cb6ced1be1be2ba586e1e02aabaf8
Ernest45 <gkgkswns71@gmail.com>
2024-04-24 22:37:14 +0900

    징검다리건너기 미완

---
+++ 알고리즘/백준repo/dp/징검다리건너기미완.java
@@ -0,0 +1,19 @@
+package 알고리즘.백준repo.dp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 징검다리건너기미완 {
+    //21317
+    // 삼삼을 캐기 위해 돌을 건너다니는데, 건너다니는 방법은 3가지
+    // 한칸 이동 +1 or 한칸 띄고 두번째 칸으로 이동 +2, 두칸을 스킵하고 세번째로 이동 +3
+    //각 점프마다 에너지를 소모하고, +3짜리 이동은 딱 한번만 가능하
+    // 에너지 아껴서 최대한 이동 많이하자
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+
+    }
+}
---
+++ 알고리즘/백준repo/백준dp추천문제/부녀회장이될테야.java
@@ -0,0 +1,2 @@
+package 알고리즘.백준repo.백준dp추천문제;public class 부녀회장이될테야 {
+}
