46b0236e2c592b03dc678086b7884c48e3c0d7db
Ernest45 <gkgkswns71@gmail.com>
2024-04-24 22:41:00 +0900

    부녀회장이될테야풀이

--- 알고리즘/백준repo/백준dp추천문제/부녀회장이될테야.java
+++ 알고리즘/백준repo/백준dp추천문제/부녀회장이될테야.java
@@ -1,2 +1,21 @@
-package 알고리즘.백준repo.백준dp추천문제;public class 부녀회장이될테야 {
+package 알고리즘.백준repo.백준dp추천문제;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 부녀회장이될테야 {
+    //2775
+    // 바로 아래층의 누적합을 구해라
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < t; i++) {
+            int k = Integer.parseInt(br.readLine());
+            int n = Integer.parseInt(br.readLine());
+        }
+            
+    }
 }
