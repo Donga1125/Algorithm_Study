d622a0e8ae0ef9ddef6b0e62b7f41432b127cd81
Ernest45 <gkgkswns71@gmail.com>
2024-04-24 22:26:30 +0900

    누적합 구하기5 2차원배열 누적합공식

--- 알고리즘/백준repo/dp/구간합구하기5성공.java
+++ 알고리즘/백준repo/dp/구간합구하기5성공.java
@@ -5,12 +5,15 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class 구간합구하기5실패 {
+public class 구간합구하기5성공 {
 
     //11660
     //2,2 -> 3,4 구하라고 하면 22 23 24 32 33 34구하기다 21은 포함안됨
 
     //현재 막히는 부분이 2차원 dp의 순서에 따라 어떻게 저장하는지 ? or 1차원 dp로 바꿔서증가 시킬지
+
+    //구간합을 구하는 공식이 있다.. arr[] 안쓰고 애초 누적합으로 저장
+    // dp[i][j] = dp[i][j - 1] + dp[i - 1][j] - dp[i - 1][j - 1]
     static int[][] arr;
 //    static int[][] wanted;
 //    static long[] dp1;
@@ -24,80 +27,52 @@ public class 구간합구하기5실패 {
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
         arr = new int[n+ 1][n+1];
+        dp = new long[n + 1][n + 1];
 
         StringBuilder sb = new StringBuilder();
 
 
 
-        from = new int[m +1][2 +1];
-        to = new int[m+1][2 + 1];
+//        from = new int[m +1][2 +1];
+//        to = new int[m+1][2 + 1];
 
 
 
         for (int i = 1; i <= n; i++) {
             st = new StringTokenizer(br.readLine());
+
             for (int j = 1; j <= n; j++) {
-                arr[i][j] = Integer.parseInt(st.nextToken());
+                dp[i][j] = dp[i][j - 1] + dp[i - 1][j] - dp[i - 1][j - 1] + Integer.parseInt(st.nextToken());
+                // 구간합을 처음부터 구해놓기
+
+
 
             }
+
+
 
             
         }
 
+        long answer;
         for (int i = 1; i <= m; i++) {
-            dp = new long[n + 1][n + 1];
-//            dp1 = new long[n + 1];
+            answer = 0;
             st = new StringTokenizer(br.readLine());
-            int x1 = 0;
-            int x2 = 0;
-            int y1 = 0;
-            int y2 = 0;
+            int x1 =Integer.parseInt(st.nextToken()); // 2,2 3,4
+            int x2 =Integer.parseInt(st.nextToken());
+            int y1 =Integer.parseInt(st.nextToken());
+            int y2 =Integer.parseInt(st.nextToken());
+            answer = dp[y1][y2] - dp[y1][x2 - 1]- dp[x1 - 1][y2]  + dp[x1 - 1][x2 - 1];
+            //누적합을 구해놓고 그 누적합에서 범위 밖 빼주고 겹치는 범위 더해주기!
 
-            for (int j = 1; j <=2; j++) {
-                from[i][j] = Integer.parseInt(st.nextToken());
+            sb.append(answer).append('\n');
 
-
-            }
-
-            x1 = from[i][1];
-            x2 = from[i][2];
-
-            for (int j = 1; j <= 2; j++) {
-                to[i][j] = Integer.parseInt(st.nextToken());
-
-            }
-            y1 = to[i][1];
-            y2 = to[i][2];
-            dp[x1][x2] = arr[x1][x2];
-
-            for (int j = x1; j <= y1 ; j++) {
-                for (int k = x2; k <= y2; k++) {
-
-                    if (k == x2 && dp[j][k] == 0) { // 첫 시작이거나 시프트 시 // arr,dp 둘다 1차원배열로 하려다 첫시작과 시프트 바꿔봄
-                        dp[j][k] = dp[j - 1][y2] + arr[j][k];
-
-                    } else
-                    dp[j][k] = dp[j][k - 1] + arr[j][k];
-
-
-
-                }
-
-
-            }
-
-
-
-            sb.append(dp[y1][y2]).append("\n");
-
-            
         }
-
         System.out.println(sb);
 
 
-
-
+            
+        }
 
     }
-}
+
