5f2cc21437884d81c0c1327bb4e1de1d1271d32a
Ernest45 <gkgkswns71@gmail.com>
2024-04-27 23:35:56 +0900

    달나라토끼

---
+++ 알고리즘/백준repo/dp/달나라토끼를위한.java
@@ -0,0 +1,15 @@
+package 알고리즘.백준repo.dp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 달나라토끼를위한 {
+    //17212
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+
+    }
+}
