056f3fbfde5e29c244137af8428b9cfb75eec840
Ernest45 <gkgkswns71@gmail.com>
2024-04-28 23:55:55 +0900

    카드구매 주석추가

--- 알고리즘/백준repo/dp/카드구매하기.java
+++ 알고리즘/백준repo/dp/카드구매하기.java
@@ -3,12 +3,22 @@ package 알고리즘.백준repo.dp;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class 카드구매하기 {
-    
+
+    static int[] card;
+
+    //카드 갯수n  1,2,3,4 있고 금액으로 배열저장하는데 n개를 사면서 금액이 가장 큰 걸 반환
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        card = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            card[i] = Integer.parseInt(st.nextToken());
+
+        }
             
     }
 }
