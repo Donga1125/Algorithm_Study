71a0bf8cf3b1e410bb4b58da097e322a9e22710c
Ernest45 <gkgkswns71@gmail.com>
2024-04-29 19:52:07 +0900

    2xn타일링2 ArraysIndexOutOfBound 해결

--- 알고리즘/백준repo/dp/투xn타일링투.java
+++ 알고리즘/백준repo/dp/투xn타일링투.java
@@ -11,12 +11,18 @@ public class 투xn타일링투 {
     // 2xn 타일링을 잘 생각해보자 마지막에 채우는 경우가 3가지인데, 1x2이 나오면 -2로 따지자
 
     private static int t;
-    private static Integer[] dp;
+    private static int[] dp;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         t = Integer.parseInt(br.readLine());
-        dp = new Integer[t + 1];
+        dp = new int[1001]; // 항상 t+1로 초기화하는데 이럴 경우 arrayIndexOutOfBound가 날 수 있다.
+        // 경계조건이나, 예치직않은 입력 방지 ,예상치못한 인덱스오버플로우 방지
+
+        for (int i = 0; i <= t; i++) {
+            dp[i] = -1;
+
+        }
 
 
         System.out.println(dp(t) % 10007);
@@ -27,11 +33,11 @@ public class 투xn타일링투 {
 
     private static int dp(int n) {
 
-        dp[0] = 1;
+
         dp[1] = 1;
         dp[2] = 3; // 2일때 경우의 수가 3가지다.
 
-        if (dp[n] == null) {
+        if (dp[n] == -1) {
             return dp[n] = (dp(n - 1) + 2 * dp(n - 2)) % 10007;
 
             //dp n-1의 경우 어차피 세로 하나밖에 못오고 dp n-2의 경우 원래 2xn문제에선 가로 하나만 왔지만,
