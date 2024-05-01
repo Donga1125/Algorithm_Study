b7d575968ac090b020b461fd9f45ace76df65be5
Ernest45 <gkgkswns71@gmail.com>
2024-05-01 23:41:26 +0900

    이동하기

---
+++ 알고리즘/백준repo/dp/이동하기.java
@@ -0,0 +1,15 @@
+package 알고리즘.백준repo.dp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 이동하기 {
+    //11048
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int f = Integer.parseInt(br.readLine());
+
+    }
+}
