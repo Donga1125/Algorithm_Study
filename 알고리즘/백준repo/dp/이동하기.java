080f0537dcec94d908feb6052cb059439c276519
Ernest45 <gkgkswns71@gmail.com>
2024-05-02 20:56:23 +0900

    이동하기 기본조건

--- 알고리즘/백준repo/dp/이동하기.java
+++ 알고리즘/백준repo/dp/이동하기.java
@@ -6,10 +6,14 @@ import java.io.InputStreamReader;
 
 public class 이동하기 {
     //11048
+    //nxm 미로에 갇혀있다. 11부터 nm까지..
+    // 움직임은 오,밑,대각오밑 세개로만 움직일 수 있으며 방문 때 마다 놓여있는 사탕을 가질수도아닐수도 있다
+    // 이동할 때 마다 최종합이 가장 큰 수를 구해서 반환해라
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int f = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
 
     }
 }
