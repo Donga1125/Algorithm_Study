59282798fe32e79d8d15fe6fa0f825c48a582b4c
Ernest45 <gkgkswns71@gmail.com>
2024-05-07 21:21:06 +0900

    내려가기

---
+++ 알고리즘/백준repo/dp/내려가기.java
@@ -0,0 +1,15 @@
+package 알고리즘.백준repo.dp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 내려가기 {
+    //2096
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int b = Integer.parseInt(br.readLine());
+
+    }
+
+}
