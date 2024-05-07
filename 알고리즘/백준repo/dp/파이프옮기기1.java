67bdfe486b7eb6b6ee0d39a45ab201fde3421920
Ernest45 <gkgkswns71@gmail.com>
2024-05-07 21:18:31 +0900

    파이프 옮기기 주석추가

--- 알고리즘/백준repo/dp/파이프옮기기1.java
+++ 알고리즘/백준repo/dp/파이프옮기기1.java
@@ -1,4 +1,15 @@
 package 알고리즘.백준repo.dp;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class 파이프옮기기1 {
+    //17070
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int f = Integer.parseInt(br.readLine());
+
+    }
 }
