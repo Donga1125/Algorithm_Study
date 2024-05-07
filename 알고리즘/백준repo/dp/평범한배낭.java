11382af14fb944f16ae848deadc4ca19b541cacc
Ernest45 <gkgkswns71@gmail.com>
2024-05-07 21:22:19 +0900

    평범한 배낭

---
+++ 알고리즘/백준repo/dp/평범한배낭.java
@@ -0,0 +1,16 @@
+package 알고리즘.백준repo.dp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 평범한배낭 {
+    //12865
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int b = Integer.parseInt(br.readLine());
+
+        System.out.println();
+
+    }
+}
