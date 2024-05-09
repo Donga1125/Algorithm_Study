49cb2fc419b6abe481fbe820955582394646164a
Ernest45 <gkgkswns71@gmail.com>
2024-05-09 21:38:56 +0900

    파이프옮기기1 조건주석

--- 알고리즘/백준repo/dp/파이프옮기기1.java
+++ 알고리즘/백준repo/dp/파이프옮기기1.java
@@ -6,6 +6,9 @@ import java.io.InputStreamReader;
 
 public class 파이프옮기기1 {
     //17070
+    // 벽이 존재할 수 있다 1로 주어짐 그럴 거긴 예외로 처리
+    // 파이프는 45도만 가능하고 초기ㅇ ㅔ놓인 조건에 따라 즉 - 이면 \,  \ 이면 ㅣ,- ㅣ이면 \
+    // 첫 파이프는 1,1 1,2를 가로의 모양으로 차지하고 있다
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
