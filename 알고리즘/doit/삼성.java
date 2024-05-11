03d34536f78361caca0b965c8ab4e1e22f9e45d3
Ernest45 <gkgkswns71@gmail.com>
2024-05-11 20:33:49 +0900

    간단한 수찾기

--- 알고리즘/doit/삼성.java
+++ 알고리즘/doit/삼성.java
@@ -1,2 +1,37 @@
-package 알고리즘.doit;public class 삼성 {
+package 알고리즘.doit;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 삼성 {
+    static int arr[][];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        arr = new int[t + 1][t + 1];
+        int ans = 0;
+
+
+        while (ans < t) {
+
+            int i = Integer.parseInt(br.readLine());
+            int j = Integer.parseInt(br.readLine());
+            arr[i][j] = 1;
+            ans++;
+
+
+        }
+        for (int k = 1; k <= t; k++) {
+            for (int l = 1; l <= t; l++) {
+                System.out.print(arr[k][l] + " ");
+
+            }
+            System.out.println();
+        }
+
+
+    }
 }
