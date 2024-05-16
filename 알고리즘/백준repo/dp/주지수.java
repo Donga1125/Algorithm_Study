036ea802a19fbd13338e4ef8931db2738c8183fc
Ernest45 <gkgkswns71@gmail.com>
2024-05-16 20:11:44 +0900

    주지수 초기값

---
+++ 알고리즘/백준repo/dp/주지수.java
@@ -0,0 +1,34 @@
+package 알고리즘.백준repo.dp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 주지수 {
+
+    //15724
+    private static int[][] land;
+    private static int answer;
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        land = new int[n + 1][m + 1];
+
+        for (int i = 1; i <= n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= m; j++) {
+                land[i][j] = Integer.parseInt(st.nextToken());
+                System.out.print(land[i][j] + " ");
+
+            }
+            System.out.println();
+        }
+
+        answer = Integer.parseInt(br.readLine());
+
+    }
+}
