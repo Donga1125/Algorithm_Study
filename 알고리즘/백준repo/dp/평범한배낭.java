027a17cc358975ce87083a66894abd9d172a10cf
Ernest45 <gkgkswns71@gmail.com>
2024-05-17 23:41:46 +0900

    평범한 배낭 주석

--- 알고리즘/백준repo/dp/평범한배낭.java
+++ 알고리즘/백준repo/dp/평범한배낭.java
@@ -3,14 +3,25 @@ package 알고리즘.백준repo.dp;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class 평범한배낭 {
+    static int[][] arr;
     //12865
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int b = Integer.parseInt(br.readLine());
+        arr = new int[b][2 + 1];
+
 
         for (int i = 0; i < b; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            for (int j = 1; j <= 2; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+
+            }
+
 
         }
 
