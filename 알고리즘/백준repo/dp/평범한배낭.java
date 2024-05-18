9a16fb92575d504b670ceaa8977fec8e1a289118
Ernest45 <gkgkswns71@gmail.com>
2024-05-18 23:57:24 +0900

    평범한 배낭 틀림

--- 알고리즘/백준repo/dp/평범한배낭.java
+++ 알고리즘/백준repo/dp/평범한배낭.java
@@ -6,6 +6,7 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class 평범한배낭 {
+    //배낭 주석
     static int[][] arr;
     //12865
     public static void main(String[] args) throws IOException {
