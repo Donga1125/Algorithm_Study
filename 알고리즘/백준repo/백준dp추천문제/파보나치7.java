7e8bc5bc34201c9165472bd5089ab9de1f0696fd
Ernest45 <gkgkswns71@gmail.com>
2024-05-19 23:40:04 +0900

    파보나치7

---
+++ 알고리즘/백준repo/백준dp추천문제/파보나치7.java
@@ -0,0 +1,14 @@
+package 알고리즘.백준repo.백준dp추천문제;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 파보나치7 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+
+    }
+}
