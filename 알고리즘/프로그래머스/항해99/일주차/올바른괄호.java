e954af4bb7d4b87db9dcd038a6f4c9e55df99552
Ernest45 <gkgkswns71@gmail.com>
2024-05-23 20:04:12 +0900

    올바른괄호 queue 써서 해결

--- 알고리즘/프로그래머스/항해99/일주차/올바른괄호.java
+++ 알고리즘/프로그래머스/항해99/일주차/올바른괄호.java
@@ -1,11 +1,14 @@
 package 알고리즘.프로그래머스.항해99.일주차;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class 올바른괄호 {
     // 올바른 괄호를 만드는 건데 (로 시작했음 )로 닫혀야 한다
     // true or false로 반환 100,000
 
-    // )로 닫혀야 하고 짝이 맞아야 한다 (로 들어와야 하고 큐 ? 스택 ?
     // 시작은 무조건 (, 끝은 ), 짝수
+    // 큐로 (면 넣고 )로 빼서 queue가 비었는 지 반환하면 될 거 같다
 
     public static void main(String[] args) {
 
@@ -19,6 +22,7 @@ public class 올바른괄호 {
     }
 
     private boolean solution(String s) {
+        Queue<Character> queue = new LinkedList<>();
         boolean answer = true;
 
         System.out.println(s.length());
@@ -29,8 +33,16 @@ public class 올바른괄호 {
             return false; // 기본 조건 짝수가 아니거나 (로 시작하지 않거나, )로 끝나지 않거나 바로 false
         }
 
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') { // (,) 둘 중 하나만 들어옴
+                queue.offer(s.charAt(i));  //add, 와 offer , remove 과 poll는 실패시 false냐 오류냐 (offer,poll을 쓰자 안정성)
 
-        return answer;
+            } else // 시작부터 닫는 괄호면 ? poll 했을 때 뺼 수 있는 게 없기에 에러가 날껀데
+                queue.poll(); // 시작을 미리 걸렀기에 )부터 실행될 일은 없다.
+        }
+
+
+        return queue.isEmpty();
     }
 
 
