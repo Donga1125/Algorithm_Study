710f6df46ef7d4f81bb8afbc249940ddc6cc437a
Ernest45 <gkgkswns71@gmail.com>
2024-05-23 21:41:56 +0900

    평범한 배낭 주석수정

--- 알고리즘/백준repo/dp/평범한배낭.java
+++ 알고리즘/백준repo/dp/평범한배낭.java
@@ -7,16 +7,22 @@ import java.util.StringTokenizer;
 
 public class 평범한배낭 {
     //배낭 주석
+
+    // 배낭의  물품 수 1<= n <=100 과 최대 무게 1<= k <= 100,000 주어지고,
+    // 무게 w(1~100,000) 가치 v(0~1,000)이 주어질 때 들 수 있는 최대 가치 합을 출력
     static int[][] arr;
+    static int k;
     //12865
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int b = Integer.parseInt(br.readLine());
-        arr = new int[b][2 + 1];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
+        arr = new int[n][2 + 1];
 
 
-        for (int i = 0; i < b; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
 
             for (int j = 1; j <= 2; j++) {
                 arr[i][j] = Integer.parseInt(st.nextToken());
