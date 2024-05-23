fe6eacbb539c3f808100ceaf27f787a914214666
Ernest45 <gkgkswns71@gmail.com>
2024-05-24 00:50:50 +0900

    올바른 괄호 틀림 ㅠ

--- 알고리즘/프로그래머스/항해99/일주차/올바른괄호.java
+++ 알고리즘/프로그래머스/항해99/일주차/올바른괄호.java
@@ -14,7 +14,7 @@ public class 올바른괄호 {
 
         올바른괄호 main = new 올바른괄호();
 
-        boolean answer = main.solution(")())()");
+        boolean answer = main.solution("))))()");
 
         System.out.println(answer);
 
@@ -23,13 +23,13 @@ public class 올바른괄호 {
 
     private boolean solution(String s) {
         Queue<Character> queue = new LinkedList<>();
-        boolean answer = true;
+
 
         System.out.println(s.length());
 
 
 
-        if (s.length() % 2 != 0 || s.charAt(s.length() - 1) != ')') {
+        if (s.length() % 2 != 0 || s.charAt(0) != '(' || s.charAt(s.length() - 1) != ')') {
             return false; // 기본 조건 짝수가 아니거나 (로 시작하지 않거나, )로 끝나지 않거나 바로 false
             // || s.charAt(s.length() - 1) != ')'
             // || s.charAt(0) != '('
@@ -37,14 +37,17 @@ public class 올바른괄호 {
 
         for (int i = 0; i < s.length(); i++) {
             if (s.charAt(i) == '(') { // (,) 둘 중 하나만 들어옴
-                queue.add(s.charAt(i));  //add, 와 offer , remove 과 poll는 실패시 false냐 오류냐 (add,remove을 쓰자 안정성)
+                queue.offer(s.charAt(i));  //add, 와 offer , remove 과 poll는 실패시 false냐 오류냐 (add,remove을 쓰자 안정성)
 
             } else // 시작부터 닫는 괄호면 ? poll 했을 때 뺼 수 있는 게 없기에 에러가 날껀데
-                queue.remove(); // 시작을 미리 걸렀기에 )부터 실행될 일은 없다.
+
+                queue.poll(); // 시작을 미리 걸렀기에 )부터 실행될 일은 없다.
+
         }
 
 
         return queue.isEmpty();
+
     }
 
 
