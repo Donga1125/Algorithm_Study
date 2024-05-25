50b5320c82bdcd71b1b61aa2354b361e5423a290
Ernest45 <gkgkswns71@gmail.com>
2024-05-25 22:14:57 +0900

    smallestNumber 제목수정 및 디렉토리이동

--- 알고리즘/프로그래머스/항해99/일주차/리트코드/smallNumberInInfiniteSet.java
+++
@@ -1,4 +0,0 @@
-package 알고리즘.프로그래머스.항해99.일주차.리트코드;
-
-public class smallNumberInInfiniteSet {
-}
---
+++ 알고리즘/프로그래머스/항해99/일주차/리트코드/smallestNumberInInfiniteSet.java
@@ -0,0 +1,6 @@
+package 알고리즘.프로그래머스.항해99.일주차.리트코드;
+
+public class smallestNumberInInfiniteSet {
+
+    // 모든 양수를 포함하는 집합이 있고,
+}
