69d66747518884a795586ae73add394a22caa6b3
Ernest45 <gkgkswns71@gmail.com>
2024-05-27 20:01:45 +0900

    H-index

---
+++ 알고리즘/프로그래머스/항해99/이주차/hindex.java
@@ -0,0 +1,15 @@
+package 알고리즘.프로그래머스.항해99.이주차;
+
+public class hindex {
+    //문제를 잘 읽어야 한다..
+    // 논문의 배열이 들어오고
+
+    public static void main(String[] args) {
+
+    }
+
+    private int solution(int[] citations) {
+        int answer = 0;
+        return answer;
+    }
+}
--- 알고리즘/프로그래머스/항해99/일주차/가장큰수compare.java
+++ 알고리즘/프로그래머스/항해99/일주차/가장큰수compare.java
@@ -16,26 +16,27 @@ public class 가장큰수compare {
 
     private String solution(int[] numbers) {
 
-    String[] strNumbers = Arrays.stream(numbers)
+    String[] strNumbers = Arrays.stream(numbers) // 정수들을 스트림으로 문자열로 변환하는 과정
             .mapToObj(String::valueOf)
             .toArray(String[]::new);
 
-    // Sort strings based on custom comparator
+
+    // 정렬 comparator 인터페이스 구현을 자리수로 비교해서 구현
         Arrays.sort(strNumbers, new Comparator<String>() {
         @Override
         public int compare(String o1, String o2) {
             String order1 = o1 + o2;
             String order2 = o2 + o1;
-            return order2.compareTo(order1); // Descending order
+            return order2.compareTo(order1);
         }
     });
 
-    // If the largest number is "0", the result is "0"
+      // 0이 들어올 수 있으니 정렬 후에 0이면 0 return;
         if (strNumbers[0].equals("0")) {
         return "0";
     }
 
-    // Join sorted strings to form the largest number
+    // 좀 더 빠르게 StringBuilder에 담아 반환
     StringBuilder result = new StringBuilder();
         for (String number : strNumbers) {
         result.append(number);
