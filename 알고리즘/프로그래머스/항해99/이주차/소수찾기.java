038943aff469418ba4736edecfc35d4ac841c391
Ernest45 <gkgkswns71@gmail.com>
2024-05-29 17:08:34 +0900

    소수찾기

---
+++ 알고리즘/프로그래머스/항해99/이주차/소수찾기.java
@@ -0,0 +1,32 @@
+package 알고리즘.프로그래머스.항해99.이주차;
+
+public class 소수찾기 {
+
+    // String으로 문자열 받고 그 String으로 만들 수 이쓴ㄴ 숫자를 구한다음 에라스토테네스의 체로 거르면
+
+    // 1. 만들 수 있는 수를 다 만들고 나서 그 이후 체로 거르기
+    // numbers는 길이 1 이상 7 이하인 문자열입니다.
+    //numbers는 0~9까지 숫자만으로 이루어져 있습니다.
+    //"013"은 0, 1, 3 숫자가 적힌 종이 조각이 흩어져있다는 의미입니다.
+
+    private int solution(String num) {
+        int answer = 0;
+
+        for (int i = 0; i < num.length(); i++) {
+            for (int j = 0; j < num.length(); j++) {
+                
+            }
+
+        }
+        return answer;
+    }
+
+    private int erasto()
+
+
+    public static void main(String[] args) {
+
+        소수찾기 main = new 소수찾기();
+
+    }
+}
