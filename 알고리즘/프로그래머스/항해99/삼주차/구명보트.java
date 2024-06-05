d85ec458c9844c139710681cc972920b212f283b
Ernest45 <gkgkswns71@gmail.com>
2024-06-05 18:47:10 +0900

    구명보트 문제

---
+++ 알고리즘/프로그래머스/항해99/삼주차/구명보트.java
@@ -0,0 +1,21 @@
+package 알고리즘.프로그래머스.항해99.삼주차;
+
+public class 구명보트 {
+
+    //무인도에 갇힌 사람이 탈출하기 위해 구명보트 사용
+    //구명 보트는 작아 2명밖에 못타고, 무게 제한도 있음
+    //예를 들어, 사람들의 몸무게가 [70kg, 50kg, 80kg, 50kg]이고
+    //구명보트의 무게 제한이 100kg이라면 2번째 사람과 4번째 사람은 같이 탈 수 있지만
+    //1번째 사람과 3번째 사람의 무게의 합은 150kg이므로 구명보트의 무게 제한을 초과하여 같이 탈 수 없습니다.
+
+    // 최대한 적게 구명보트 사용해서 탈출
+    //사람들의 몸무게를 담은 배열 people과 구명보트의 무게 제한 limit가 매개변수로 주어짐
+
+    public static void main(String[] args) {
+
+    }
+    private int solution(int[] people, int limit) {
+        int answer = 0;
+        return answer;
+    }
+}
