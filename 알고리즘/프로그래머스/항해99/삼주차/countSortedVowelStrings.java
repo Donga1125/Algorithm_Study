b1d7622373e41e08c9f8c766d3524889f151fc25
Ernest45 <gkgkswns71@gmail.com>
2024-06-07 23:21:52 +0900

    countSortedVowelStrings dp 방법

--- 알고리즘/프로그래머스/항해99/삼주차/countSortedVowelStrings.java
+++ 알고리즘/프로그래머스/항해99/삼주차/countSortedVowelStrings.java
@@ -1,5 +1,8 @@
 package 알고리즘.프로그래머스.항해99.삼주차;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class countSortedVowelStrings {
 
     //1641. Count Sorted Vowel Strings
@@ -18,6 +21,9 @@ public class countSortedVowelStrings {
     //dp 문제인데 aeiou라면 조합 문제인데 중복이 가능하니 중복 조합 ex)5H2 = n+r-1Cr = 6C2 = 15개
 
     private static char[] vowel = new char[] {'a', 'e', 'i', 'o', 'u'};
+    private static int[][] dp;
+
+    private static HashMap<Integer, String> mapDp = new HashMap<>();
 
     public static void main(String[] args) {
 
@@ -25,14 +31,27 @@ public class countSortedVowelStrings {
     public int countVowelStrings(int n) {
         //top-down으로 하면
 
-        for (int i = 0; i <; i++) {
-            for (int j = i +1; j <; j++) {
+        dp = new int[n + 1][5];
+
+        dp[1][1] = 1; // a
+        dp[1][2] = 1; // e
+        dp[1][3] = 1; // i
+        dp[1][4] = 1; // o
+        dp[1][5] = 1; // u
+
+        // 하나의 모음의 경우 a,e,i,o,u로 초기화 직관적이기 위해 하나씩 넣어줌
+
+        for (int i = 2; i <= n +1; i++) {
+
+            for (int j = i; j <= 5; j++) {
+                // dp[j] = dp[i] + dp[j]  aa + a , aa + e , aa
+                dp[i][j] = dp[i - 1][j] + dp[i][j];
+
 
             }
         }
 
 
-        return 5;
 
     }
 
