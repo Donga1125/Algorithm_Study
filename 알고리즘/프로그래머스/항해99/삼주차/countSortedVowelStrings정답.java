a54811c3affba5b38479515bf76732e4b4bd039e
Ernest45 <gkgkswns71@gmail.com>
2024-06-08 20:18:53 +0900

    countSort dp 정리

--- 알고리즘/프로그래머스/항해99/삼주차/countSortedVowelStrings정답.java
+++ 알고리즘/프로그래머스/항해99/삼주차/countSortedVowelStrings정답.java
@@ -6,25 +6,36 @@ public class countSortedVowelStrings정답 {
     private static char[] vowel = new char[] {'a', 'e', 'i', 'o', 'u'};
 
     public static void main(String[] args) {
-        int n = 33;
+        int n = 7;
         System.out.println(countVowelStrings(n));  // 예시 테스트
     }
 
     public static int countVowelStrings(int n) {
         // dp 배열 초기화
         int[][] dp = new int[n + 1][5];
+        int[] dp1 = new int[]{1, 1, 1, 1, 1};
 
-        // 길이가 1인 경우 초기화
+//        for (int i = 1; i < n; i++) {
+//            for (int j = 4; j >= 0; j--) {
+//                dp1[j] = dp1[j] + dp1[j + 1];
+//
+//            }
+//        }
+//
+//        return dp1[0] + dp1[1] + dp1[2] + dp1[3] + dp1[4];
+
+//         길이가 1인 경우 초기화
         for (int j = 0; j < 5; j++) {
             dp[1][j] = 1;
         }
 
         // DP 테이블 채우기
         for (int i = 2; i <= n; i++) {
+
             for (int j = 0; j < 5; j++) {
-                dp[i][j] = 0;
+
                 for (int k = j; k < 5; k++) {
-                    dp[i][j] += dp[i - 1][k];
+                    dp[i][j] = dp[i][j] + dp[i - 1][k];
                 }
             }
         }
