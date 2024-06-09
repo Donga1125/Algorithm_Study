380818570d821f2faa94647703bf485ccca14d8d
Ernest45 <gkgkswns71@gmail.com>
2024-06-09 20:34:04 +0900

    countSquareSubMatric

---
+++ 알고리즘/프로그래머스/항해99/삼주차/countSquareSubmatriceswithAllOnes.java
@@ -0,0 +1,12 @@
+package 알고리즘.프로그래머스.항해99.삼주차;
+
+public class countSquareSubmatriceswithAllOnes {
+
+    // 0 or 1의 수가 주어지고 arr[][]안에 주어지면
+    //정사각형의 갯수 return
+
+    public static void main(String[] args) {
+
+    }
+
+}
