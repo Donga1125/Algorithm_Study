0cb30fbf14f983117339bec91bb0403a9b917489
Ernest45 <gkgkswns71@gmail.com>
2024-06-17 01:06:27 +0900

    groupthePeople

---
+++ 알고리즘/프로그래머스/항해99/사주차/groupthePeopleGiventheGroupSizeTheyBelongTo.java
@@ -0,0 +1,25 @@
+package 알고리즘.프로그래머스.항해99.사주차;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class groupthePeopleGiventheGroupSizeTheyBelongTo {
+
+
+    public static void main(String[] args) {
+
+    }
+
+
+
+
+
+    public List<List<Integer>> groupThePeople(int[] groupSizes) {
+
+        List<List<Integer>> answer = new ArrayList<>();
+
+
+        return answer;
+
+    }
+}
