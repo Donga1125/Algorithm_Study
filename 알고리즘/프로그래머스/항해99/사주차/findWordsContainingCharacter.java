c0d054c5076a139196584d5d6f45b258fc479ba6
Ernest45 <gkgkswns71@gmail.com>
2024-06-16 23:44:05 +0900

    findwordsContainingChar

--- 알고리즘/프로그래머스/항해99/사주차/findWordsContainingCharacter.java
+++ 알고리즘/프로그래머스/항해99/사주차/findWordsContainingCharacter.java
@@ -1,8 +1,14 @@
 package 알고리즘.프로그래머스.항해99.사주차;
 
+import java.util.List;
+
 public class findWordsContainingCharacter {
 
     public static void main(String[] args) {
+
+    }
+
+    public List<Integer> findWordsContaining(String[] words, char x) {
 
     }
 }
