c61406e32d828e43360b3979987421e16ebad9d7
Ernest45 <gkgkswns71@gmail.com>
2024-06-18 00:09:17 +0900

    combinationITerator 틀린답

---
+++ 알고리즘/프로그래머스/항해99/오주차/combinationIteratorAnswer.java
@@ -0,0 +1,105 @@
+package 알고리즘.프로그래머스.항해99.오주차;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class combinationIteratorAnswer {
+
+
+//    1286. Iterator for Combination
+
+    //String 문자열이 영어로 주어지는데, 사전 순으로 combi로 반환
+    // 들어온 숫자만큼 짤라서 반환하면 됨
+    // 순서 x 중복 상관 x 조합
+    // 순서가 중요하고 반환도 사전 순서임 ab는 되지만 ba는 안된다는 뜻이고
+    // ab -> bc가 아닌 ac가 나오야 사전순서
+    // 다 만들어놓고 반환하자
+
+    //next는 다음 조합된 요소를 반환
+    //hasNext는 다음 조합 요소가 있으면 t,f 반환
+
+
+
+    private List<String> list = new ArrayList<>();
+    private int now = 0;
+
+    String characters = " ";
+    int combiLength;
+
+
+
+
+    public static void main(String[] args) {
+        String ch = "chp";
+        int combi = 1;
+        String test = "abc";
+        int comm = 2;
+//        combinationIterator정답 main = new combinationIterator정답(ch, combi);
+        combinationIterator main = new combinationIterator(test, comm);
+        System.out.println(main.hasNext());
+        System.out.println(main.next());
+        System.out.println(main.hasNext());
+        System.out.println(main.hasNext());
+        System.out.println(main.next());
+        System.out.println(main.next());
+        System.out.println(main.hasNext());
+        System.out.println(main.hasNext());
+        System.out.println(main.hasNext());
+    }
+
+
+    public combinationIteratorAnswer(String characters, int combinationLength) {
+        this.characters = characters;
+        this.combiLength = combinationLength;
+
+
+        generateCombinations(new StringBuilder(), 0);
+
+
+        
+    }
+
+    private void generateCombinations(StringBuilder current, int start) {
+        
+        if (current.length() == combiLength) {
+            list.add(current.toString());
+            return;
+        }
+
+        for (int i = start; i < characters.length(); i++) {
+            current.append(characters.charAt(i));
+            generateCombinations(current, i + 1);
+            current.deleteCharAt(current.length() - 1);
+
+        }
+        
+    }
+
+
+    public String next() {
+
+        // 호출 시 하나씩 반환
+
+
+
+        String next = list.get(now++);
+//        String next = la.get(now++);
+
+
+        return next;
+
+    }
+
+    public boolean hasNext() {
+
+
+        return combiLength < list.size();
+        // size가 더 작으면 true
+        // 5 < 10
+        // 11 < 10 이면 False
+
+
+
+    }
+
+}
