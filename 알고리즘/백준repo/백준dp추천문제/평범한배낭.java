92e1b4eb9b19cc52501bb9c87df7a33a2c9f0dd2
Ernest45 <gkgkswns71@gmail.com>
2024-06-18 03:37:42 +0900

    평범한 배낭 업데이트

--- 알고리즘/백준repo/백준dp추천문제/평범한배낭.java
+++ 알고리즘/백준repo/백준dp추천문제/평범한배낭.java
@@ -35,21 +35,34 @@ public class 평범한배낭 {
 
         dp[0][0] = 0;
 
-        for (int i = 1; i <= n; i++) {
+        //현재 계산의 경우 2가지다.
+
+        //이번 걸 가지고 dp에 저장하냐 or 이번 걸 가지지 않냐
+        // 이번 걸 가지지 않는 경우에는 전에꺼 그대로 가면 되고,
+        // 만약 이번 걸 선택한다면 ?
+        // 경우를 따져야 한다. 이번걸 +했을 때 무게가 넘지 않아야하고
+        // 무게가 넘지 않을 경우에는
+
+        for (int i = 1; i <= n; i++) { //아이템 1부터 들어온4까지 탐색
             int weight = pack[i][0];
             int value = pack[i][1];
-            for (int j = 0; j <= k; j++) {
+            for (int j = 0; j <= k; j++) { //0부터 7kg 까지 탐색 1,2,3,4~ 쭉쭉 저장
 
-                dp[i][j] = dp[i - 1][j];// 이번 탐에 선택 안하면 그냥 그대로 이월
+                 //현재 무게에서 weight보다 작으면 그냥 선
+                    dp[i][j] = dp[i - 1][j];// 이번 탐에 선택 안하면 그냥 그대로 이월
 
-                if (j >= weight) { //j는 인덱스인데 ? 현재 인덱스가 무게를 넘지 않으면 ?
-                    System.out.print(dp[i][j] + " ");
-                    dp[i][j] = Math.max(dp[i][j], dp[i - 1][j - weight] +value);
+
+
+
+
+                if (j >= weight) { //j는 인덱스인데 ? 현재 인덱스가 무게를 넘지 않으면 ? 아
+                    // j가 배낭의 무게인거고, weight가 현재 아이템의 무게
+
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i - 1][j - weight] +value);
 
                 }
                 
             }
-            System.out.println();
 
         }
         System.out.println(dp[n][k]);
