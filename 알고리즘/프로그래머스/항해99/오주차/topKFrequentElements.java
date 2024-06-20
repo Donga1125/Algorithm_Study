5bcee982d01902400f68bfdfc08eb5c0e90f50b7
Ernest45 <gkgkswns71@gmail.com>
2024-06-20 20:12:16 +0900

    topKFrequentElements

---
+++ 알고리즘/프로그래머스/항해99/오주차/topKFrequentElements.java
@@ -0,0 +1,17 @@
+package 알고리즘.프로그래머스.항해99.오주차;
+
+public class topKFrequentElements {
+
+    //347. Top K Frequent Elements
+
+
+    public static void main(String[] args) {
+
+    }
+
+    public int[] topKFrequent(int[] nums, int k) {
+
+        return nums;
+
+    }
+}
