16f3b3c8a5cd01906ec177362fa991897dfb50f7
Ernest45 <gkgkswns71@gmail.com>
2024-06-22 23:55:54 +0900

    findWinner of the circullarGame leetcode

---
+++ 알고리즘/프로그래머스/항해99/오주차/findtheWinneroftheCircularGame.java
@@ -0,0 +1,17 @@
+package 알고리즘.프로그래머스.항해99.오주차;
+
+public class findtheWinneroftheCircularGame {
+
+    public static void main(String[] args) {
+
+    }
+
+
+
+    public int findTheWinner(int n, int k) {
+
+    }
+
+
+
+}
