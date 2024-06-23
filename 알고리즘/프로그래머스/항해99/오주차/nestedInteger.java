d948f8fe14f6ab4ce0f4bde0b534f9357fc5f2b6
Ernest45 <gkgkswns71@gmail.com>
2024-06-24 02:36:43 +0900

    nestedInteger 인터페이스 구현

---
+++ 알고리즘/프로그래머스/항해99/오주차/nestedInteger.java
@@ -0,0 +1,12 @@
+package 알고리즘.프로그래머스.항해99.오주차;
+
+import java.util.List;
+
+public interface nestedInteger {
+
+    public boolean isInteger();
+
+    public Integer getInteger();
+
+    public List<nestedInteger> getList();
+}
