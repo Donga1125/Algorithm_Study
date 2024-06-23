7e7fe124ea36171d19312d84d3fead25eaea175d
Ernest45 <gkgkswns71@gmail.com>
2024-06-24 02:41:02 +0900

    nestedIterator 클래스 명 변ㄱ녕

--- 알고리즘/프로그래머스/항해99/오주차/NestedInteger.java
+++ 알고리즘/프로그래머스/항해99/오주차/NestedInteger.java
@@ -2,11 +2,11 @@ package 알고리즘.프로그래머스.항해99.오주차;
 
 import java.util.List;
 
-public interface nestedInteger {
+public interface NestedInteger {
 
     public boolean isInteger();
 
     public Integer getInteger();
 
-    public List<nestedInteger> getList();
+    public List<NestedInteger> getList();
 }
---
+++ 알고리즘/프로그래머스/항해99/오주차/NestedIntegerImpl.java
@@ -0,0 +1,21 @@
+package 알고리즘.프로그래머스.항해99.오주차;
+
+import java.util.List;
+
+public class NestedIntegerImpl implements NestedInteger {
+
+    @Override
+    public boolean isInteger() {
+        return false;
+    }
+
+    @Override
+    public Integer getInteger() {
+        return null;
+    }
+
+    @Override
+    public List<NestedInteger> getList() {
+        return null;
+    }
+}
