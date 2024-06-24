31dcf1382c31a1f79355d587ed762bfbdaef94b6
Ernest45 <gkgkswns71@gmail.com>
2024-06-24 18:59:59 +0900

    Removing stars from a String leetCode2390 주석

--- 알고리즘/항해99/육주차/RemovingStarsFromAString.java
+++ 알고리즘/항해99/육주차/RemovingStarsFromAString.java
@@ -3,6 +3,11 @@ package 알고리즘.항해99.육주차;
 public class RemovingStarsFromAString {
 
     // 2390. Removing Stars From a String
+    // String이 주어지면 그 안에 있는 *가 들어온 만큼 왼쪽 char를 지우기
+    // 전부 다 유효하다.
+
+    // Input: s = "leet**cod*e"
+    //Output: "lecoe"
 
 
 
