e40404f57b0a68b480bb1b3a5352a816e0e0fac0
Ernest45 <gkgkswns71@gmail.com>
2024-06-24 18:47:38 +0900

    /NestedIntegerImp 구현체

--- 알고리즘/프로그래머스/항해99/오주차/NestedIntegerImpl.java
+++ 알고리즘/프로그래머스/항해99/오주차/NestedIntegerImpl.java
@@ -3,19 +3,36 @@ package 알고리즘.프로그래머스.항해99.오주차;
 import java.util.List;
 
 public class NestedIntegerImpl implements NestedInteger {
+    private Integer value;
+    private List<NestedInteger> list;
+
+
+    public NestedIntegerImpl(Integer value) {
+        this.value = value;
+        this.list = null;
+    }
+
+    public NestedIntegerImpl(List<NestedInteger> list) {
+        this.value = null;
+        this.list = list;
+    }
+
+
+
 
     @Override
     public boolean isInteger() {
-        return false;
+        return value != null;
     }
 
     @Override
     public Integer getInteger() {
-        return null;
+        return value;
     }
 
     @Override
     public List<NestedInteger> getList() {
-        return null;
+        return list;
     }
+
 }
