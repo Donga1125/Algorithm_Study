24aad30899b5d2554d40344dedd14372e79f1071
Ernest45 <gkgkswns71@gmail.com>
2024-06-26 18:57:07 +0900

    SeatReservationManager 문제

---
+++ 알고리즘/항해99/육주차/SeatManager.java
@@ -0,0 +1,22 @@
+package 알고리즘.항해99.육주차;
+
+public class SeatManager {
+
+    // 1845. Seat Reservation Manager
+
+    //3개의 메서드
+
+
+    public SeatManager(int n) {
+
+    }
+
+    public int reserve() {
+        return 1;
+
+    }
+
+    public void unreserve(int seatNumber) {
+
+    }
+}
--- 알고리즘/항해99/육주차/SeatReservationManager.java
+++
@@ -1,2 +0,0 @@
-package 알고리즘.항해99.육주차;public class SeatReservationManager {
-}
