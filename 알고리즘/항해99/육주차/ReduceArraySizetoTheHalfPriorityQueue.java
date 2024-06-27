051bd5afeb326da8694c4a72464b033052993508
Ernest45 <gkgkswns71@gmail.com>
2024-06-27 22:06:40 +0900

    reduceArray priorityqueue 간소화

--- 알고리즘/항해99/육주차/ReduceArraySizetoTheHalfPriorityQueue.java
+++ 알고리즘/항해99/육주차/ReduceArraySizetoTheHalfPriorityQueue.java
@@ -1,6 +1,8 @@
 package 알고리즘.항해99.육주차;
 
 import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
 
 public class ReduceArraySizetoTheHalfPriorityQueue {
 
@@ -11,13 +13,14 @@ public class ReduceArraySizetoTheHalfPriorityQueue {
 
     //arr = [3,3,3,3,5,5,5,2,2,7]
     // set을 사용해서 하나씩 담는데, 조합되는 수 를 찾는ㄷ ㅏ?
-    // map으로 갯수를 반환 ? 3 =4 5=3 2=2 7=1
+    // map으로 찾고, 그 수를 돌면서 반 이상되면 바로 리턴 ? 3 =4 5=3 2=2 7=1
     // 키 값은 상관 없음 번호로 부여하고, 몇개인지가 중요
 
     public static void main(String[] args) {
         int arr[] = new int[]{3, 3, 3, 3, 5, 5, 5, 2, 2, 7};
+        int arr1[] = new int[]{1, 9};
         ReduceArraySizetoTheHalfPriorityQueue main = new ReduceArraySizetoTheHalfPriorityQueue();
-        System.out.println(main.minSetSize(arr));
+        System.out.println(main.minSetSize(arr1));
     }
 
 
@@ -30,6 +33,26 @@ public class ReduceArraySizetoTheHalfPriorityQueue {
         for (int i = 0; i < arr.length; i++) {
             map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
         }
+
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a, b) -> b - a);
+
+
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            maxHeap.add(entry.getValue());
+        }
+
+        int sum = 0;
+        int count = 0;
+        int halfSize = arr.length / 2;
+        System.out.println(maxHeap);
+
+
+        while (sum < halfSize) {
+            sum += maxHeap.poll();
+            count++;
+        }
+
+        return count;
 
     }
 }
