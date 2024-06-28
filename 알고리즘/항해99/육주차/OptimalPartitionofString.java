15e5979c9966224cdf1c1a876207580663ca0cf7
Ernest45 <gkgkswns71@gmail.com>
2024-06-28 22:10:43 +0900

    OptimalPartitionofString 그리디 비트연산자 추가

--- 알고리즘/항해99/육주차/OptimalPartitionofString.java
+++ 알고리즘/항해99/육주차/OptimalPartitionofString.java
@@ -10,7 +10,7 @@ public class OptimalPartitionofString {
     // 최소의 subString 갯수를 반환
     // "abacaba" 라면 "ab" "ac" ,"aba"로 나눴다면  aba에 a가 2개라 안됨
     // s = abacaba 라면 앞에서부터 시작해서 있는 수 중 같은 수가 들어오면 그전까지 담기 ?
-    // 각 substring은 중복되도 되는 듯 ? 이걸 안 적어주니 헷갈리네
+    // 각 substring은 중복되도 되는 듯 하다
 
 
     public static void main(String[] args) {
@@ -43,5 +43,25 @@ public class OptimalPartitionofString {
         return count +1; // 마지막 subString이 하나 남아있으니 그 수 +1
 
     }
+
+    public int partitionString1(String s) {
+
+        int count = 0;
+        int mask = 0; // 비트마스크를 초기화합니다.
+
+        for (int i = 0; i < s.length(); i++) {
+            int currentBit = 1 << (s.charAt(i) - 'a'); // 현재 문자의 비트를 설정합니다.
+
+            if ((mask & currentBit) != 0) { // 현재 문자가 이미 비트마스크에 있는지 확인합니다.
+                count++; // 새로운 부분 문자열을 시작해야 하므로 count를 증가시킵니다.
+                mask = 0; // 비트마스크를 초기화합니다.
+            }
+
+            mask |= currentBit; // 현재 문자를 비트마스크에 추가합니다.
+        }
+
+        return count + 1; // 마지막 부분 문자열을 포함하기 위해 1을 추가합니다.
+    }
+
 
 }
