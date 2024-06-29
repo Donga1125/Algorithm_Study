8ab5ec747d5be81d962d592e72adff056e319c0e
Ernest45 <gkgkswns71@gmail.com>
2024-06-29 23:39:32 +0900

    addTwoNumbers

--- 알고리즘/항해99/육주차/AddTwoNumbers.java
+++ 알고리즘/항해99/육주차/AddTwoNumbers.java
@@ -1,2 +1,23 @@
-package 알고리즘.항해99.육주차;public class AddTwoNumbers {
+package 알고리즘.항해99.육주차;
+
+public class AddTwoNumbers {
+
+
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+        ListNode ad = new ListNode();
+
+        return ad;
+
+    }
+
+
+    public class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
 }
