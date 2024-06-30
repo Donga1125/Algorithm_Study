e658e0f6e5aae87b8f3d74ef956a1cc2dee5d2a9
Ernest45 <gkgkswns71@gmail.com>
2024-06-30 23:48:57 +0900

    addTwoNum 해법

--- 알고리즘/항해99/육주차/AddTwoNumbers.java
+++ 알고리즘/항해99/육주차/AddTwoNumbers.java
@@ -2,22 +2,39 @@ package 알고리즘.항해99.육주차;
 
 public class AddTwoNumbers {
 
-
-
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-
-        ListNode ad = new ListNode();
-
-        return ad;
+    public static void main(String[] args) {
 
     }
 
 
-    public class ListNode {
-      int val;
-      ListNode next;
-      ListNode() {}
-      ListNode(int val) { this.val = val; }
-      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-  }
-}
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+        ListNode node = new ListNode(0);
+        ListNode result = node;
+        int sum = 0;
+
+        while (l1 != null || l2 != null || sum > 0) {
+            if (l1 != null) {
+                sum += l1.val;
+                l1 = l1.next;
+            }
+
+            if (l2 != null) {
+                sum += l2.val;
+                l2 = l2.next;
+            }
+
+            node.next = new ListNode(sum % 10);
+            sum /= 10;
+
+            node = node.next;
+        }
+
+        return result.next;
+    }
+
+    }
+
+
+    }
