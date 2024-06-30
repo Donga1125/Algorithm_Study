bf20d70409711b888ee35aca77a1d995d96fa485
Ernest45 <gkgkswns71@gmail.com>
2024-06-30 23:48:44 +0900

    addTwoNum을 위한 리스트클래스  ListNOde

---
+++ 알고리즘/항해99/육주차/ListNode.java
@@ -0,0 +1,21 @@
+package 알고리즘.항해99.육주차;
+
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+
+    }
+
+    ListNode(int val) {
+
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+
+        this.val = val;
+        this.next = next;
+    }
+}
