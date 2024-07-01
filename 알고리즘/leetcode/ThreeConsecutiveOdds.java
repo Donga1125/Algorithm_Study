a7361852ac28c463320d5b4beefdb96f1a54641a
Ernest45 <gkgkswns71@gmail.com>
2024-07-01 23:45:36 +0900

    Three Consecutive Odds

--- 알고리즘/leetcode/ThreeConsecutiveOdds.java
+++ 알고리즘/leetcode/ThreeConsecutiveOdds.java
@@ -3,4 +3,12 @@ package 알고리즘.leetcode;
 public class ThreeConsecutiveOdds {
 
     //Three Consecutive Odds
+
+    public static void main(String[] args) {
+
+    }
+
+    public boolean threeConsecutiveOdds(int[] arr) {
+
+    }
 }
