ebf00e169204fa93c6d8ef66add2b71df05700e6
Ernest45 <gkgkswns71@gmail.com>
2024-07-04 20:12:58 +0900

    /MergeNodesinBetweenZeros node 연결

--- 알고리즘/leetcode/MergeNodesinBetweenZeros.java
+++ 알고리즘/leetcode/MergeNodesinBetweenZeros.java
@@ -25,13 +25,12 @@ public class MergeNodesinBetweenZeros {
     /**
      * You are given the head of a linked list, which contains
      * a series of integers separated by 0's. The beginning and end of the linked list will have Node.val == 0.
-     *
+     * <p>
      * For every two consecutive 0's,
      * merge all the nodes lying in between them into a single node whose value is the sum of all the merged nodes.
      * The modified list should not contain any 0's.
-     *
+     * <p>
      * Return the head of the modified linked list.
-
      */
 
 
@@ -40,10 +39,13 @@ public class MergeNodesinBetweenZeros {
         //0,3,1,0,4,5,2,0]
 
 
-        ListNode node = new ListNode(0,new ListNode(3));
-        ListNode node1 = new ListNode();
+        ListNode node = new ListNode
+                (0, new ListNode(3, new ListNode(1, new ListNode
+                        (0, new ListNode(4, new ListNode(5, new ListNode(2, new ListNode(0))))))));
 
 
+        MergeNodesinBetweenZeros main = new MergeNodesinBetweenZeros();
+        main.mergeNodes(node);
 
 
 
@@ -51,8 +53,44 @@ public class MergeNodesinBetweenZeros {
 
     public ListNode mergeNodes(ListNode head) {
 
+        int notFirst = 0;
+        int nowCount = 0;
 
-        return new ListNode();
+        ListNode answer = new ListNode();
+
+        while (true) { // 마지막 0은 ?
+            // 다시 잘 생각해보면 3가지 동작
+            // 현재 수가 0이고, 다음 수가 있으면  nowCount를  초기화
+
+            // 현재 수가 0이고, 다음 수가 없으면  break ?
+
+            // 다음 수가 0일 때 현재까지의 NowCount를 answer의 next에 넣어준다. (이때 초기화해도 되고, 더해줘도 된다)
+
+
+            if (head.val == 0 && head.next.val != 0) { // 자료 구조의 값을
+
+                nowCount = 0; // 현재 수가 0이고, 다음 수가 있으면  nowCount를  초기화 // 첫0 시작의 0의 경우
+
+
+            } else if (head.val == 0 && head.next == null) { // 현재 수가 0이고, 다음 수가 아예 없음 break;
+
+                break;
+
+            } else if (head.next.val == 0) { // 다음 수가 0이면 nowCOunt를 answer에 넣어주기
+                nowCount = nowCount + head.val;
+
+                answer.next = new ListNode(nowCount);
+                // 다음 주소 값 연결을 어떻게 하지 ?
+                //answr[
+
+
+            }
+
+
+        }
+
+
+        return answer;
 
     }
 
