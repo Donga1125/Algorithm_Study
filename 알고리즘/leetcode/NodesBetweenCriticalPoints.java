0621bded96d2d17710e3e565023eea2136a77ed9
Ernest45 <gkgkswns71@gmail.com>
2024-07-05 21:32:07 +0900

    NodesBetweenCriticalPoints문제 ㅊ가

--- 알고리즘/leetcode/NodesBetweenCriticalPoints.java
+++ 알고리즘/leetcode/NodesBetweenCriticalPoints.java
@@ -1,2 +1,29 @@
-package 알고리즘.leetcode;public class NodesBetweenCriticalPoints {
+package 알고리즘.leetcode;
+
+public class NodesBetweenCriticalPoints {
+
+    //2058. Find the Minimum and Maximum Number of Nodes Between Critical Points
+
+    public static void main(String[] args) {
+
+        ListNode node = new ListNode(5, new ListNode(3, new ListNode(1, new ListNode(2, new ListNode(5,
+                new ListNode(1, new ListNode(2)))))));
+
+                //5,3,1,2,5,1,2
+
+        NodesBetweenCriticalPoints main = new NodesBetweenCriticalPoints();
+        main.nodesBetweenCriticalPoints(node);
+
+
+    }
+
+
+    public int[] nodesBetweenCriticalPoints(ListNode head) {
+
+        int[] arr = new int[5];
+
+
+        return arr;
+
+    }
 }
