bf4c8f77a72400d8fdaf2613c462888ce4a090a3
Ernest45 <gkgkswns71@gmail.com>
2024-07-08 01:09:23 +0900

    numWaterBottles문제

---
+++ 알고리즘/leetcode/WaterBottles.java
@@ -0,0 +1,16 @@
+package 알고리즘.leetcode;
+
+public class WaterBottles {
+
+    //1518. Water Bottles
+
+    public static void main(String[] args) {
+
+    }
+
+    public int numWaterBottles(int numBottles, int numExchange) {
+
+
+        return 0;
+    }
+}
