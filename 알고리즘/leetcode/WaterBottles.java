ebde50dafc16a88a5ff0d65a7f37410293c75c63
Ernest45 <gkgkswns71@gmail.com>
2024-07-09 23:26:28 +0900

    numWaterBottles문제2

--- 알고리즘/leetcode/WaterBottles.java
+++ 알고리즘/leetcode/WaterBottles.java
@@ -5,12 +5,26 @@ public class WaterBottles {
     //1518. Water Bottles
 
     public static void main(String[] args) {
+        WaterBottles main = new WaterBottles();
+        main.numWaterBottles(1, 3);
 
     }
 
     public int numWaterBottles(int numBottles, int numExchange) {
 
-        //리트코드
+        //리트코ㅈ
+        int i = 0, sum = 0;
+
+        while (i < 10) {
+            i++;
+            if (i % 2 == 1) {
+                continue;
+
+            }
+            sum = sum + i;
+        }
+
+        System.out.println(sum);
 
 
         return 0;
