200e5b1127ee469322030c6c6a22aeff0158fde8
Ernest45 <gkgkswns71@gmail.com>
2024-07-09 23:52:07 +0900

    averageWaitingTime문제 추가

---
+++ 알고리즘/leetcode/AverageWaitingTime.java
@@ -0,0 +1,17 @@
+package 알고리즘.leetcode;
+
+public class AverageWaitingTime {
+
+    // 1701. Average Waiting Time
+
+    public static void main(String[] args) {
+
+    }
+
+    public double averageWaitingTime(int[][] customers) {
+        double da = 2.3;
+
+        return da;
+
+    }
+}
