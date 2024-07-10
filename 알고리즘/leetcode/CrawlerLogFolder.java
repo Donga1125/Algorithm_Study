61cc9eaf78e25a124ea77407d90355be53c3d92c
Ernest45 <gkgkswns71@gmail.com>
2024-07-10 23:54:50 +0900

    Crawler Log Folder

---
+++ 알고리즘/leetcode/CrawlerLogFolder.java
@@ -0,0 +1,17 @@
+package 알고리즘.leetcode;
+
+public class CrawlerLogFolder {
+
+    // 1598. Crawler Log Folder
+
+    public static void main(String[] args) {
+
+    }
+
+
+    public int minOperations(String[] logs) {
+
+        return 1;
+
+    }
+}
