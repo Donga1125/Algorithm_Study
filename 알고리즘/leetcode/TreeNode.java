264c9e19ddd595841d7acb38881098ee00871bc1
Ernest45 <gkgkswns71@gmail.com>
2024-07-16 19:50:36 +0900

    TreeNode 클래스 생성

---
+++ 알고리즘/leetcode/TreeNode.java
@@ -0,0 +1,22 @@
+package 알고리즘.leetcode;
+
+public class TreeNode {
+
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+
+    }
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+
+    public TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
