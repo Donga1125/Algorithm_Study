04afbf92ba1b9f1bea6918bda46becacf3f33a94
Ernest45 <gkgkswns71@gmail.com>
2024-07-16 19:50:55 +0900

    StepByStepDirectionsFromaBinaryTreeNodetoAnother 문제 추가

---
+++ 알고리즘/leetcode/july/StepByStepDirectionsFromaBinaryTreeNodetoAnother.java
@@ -0,0 +1,28 @@
+package 알고리즘.leetcode.july;
+
+public class StepByStepDirectionsFromaBinaryTreeNodetoAnother {
+
+    // 2096. Step-By-Step Directions From a Binary Tree Node to Another
+
+
+    //n개의 노드를 가진 이진트리가 주어지고, 각자 고유한 번호를 1~n까지 부여
+    // 시작 노드부터 도착 노드까지의 최단 거리로 움직이는데,
+    // U = from node to parent node
+    // R = from node to right child node
+    // L = from node to left child node
+
+    // return은 최단거리로 움직 였을 때의 사용된 String을 반환
+
+    public static void main(String[] args) {
+
+    }
+
+
+    public String getDirections(TreeNode root, int startValue, int destValue) {
+
+        return "";
+
+    }
+
+
+}
