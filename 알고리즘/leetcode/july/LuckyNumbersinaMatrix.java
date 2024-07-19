764d7cbf8d4e510d9fe32ff7c98c53da6521fe60
Ernest45 <gkgkswns71@gmail.com>
2024-07-19 22:14:27 +0900

    1380 Lucky Numbers in a Matrix 문제 추가

--- 알고리즘/leetcode/july/LuckyNumbersinaMatrix.java
+++ 알고리즘/leetcode/july/LuckyNumbersinaMatrix.java
@@ -1,2 +1,17 @@
-package 알고리즘.leetcode.july;public class LuckyNumbersinaMatrix {
+package 알고리즘.leetcode.july;
+
+import java.util.List;
+
+public class LuckyNumbersinaMatrix {
+
+    // 1380 Lucky Numbers in a Matrix
+
+    public static void main(String[] args) {
+
+    }
+
+
+    public List<Integer> luckyNumbers (int[][] matrix) {
+
+    }
 }
