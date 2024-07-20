456795cff39787f36bcd7b935b43b467e2c43882
Ernest45 <gkgkswns71@gmail.com>
2024-07-20 23:53:48 +0900

    Find Valid Matrix Given Row and Column Sums 테스트케이스 추가

--- 알고리즘/leetcode/july/FindValidMatrixGivenRowandColumnSums.java
+++ 알고리즘/leetcode/july/FindValidMatrixGivenRowandColumnSums.java
@@ -4,13 +4,18 @@ public class FindValidMatrixGivenRowandColumnSums {
 
     //1605. Find Valid Matrix Given Row and Column Sums
 
+    //음수가 아닌 행의 합과 열의 합이 주어지는데, 그 수로 기존 배열을 만들어라
+
+
     public static void main(String[] args) {
+        int[] rowSum = new int[]{5, 7, 10};
+        int[] colSum = new int[]{8, 6, 8};
 
     }
 
     int[][] restoreMatrix(int[] rowSum, int[] colSum) {
 
-        int[][] answer = new int[][];
+        int[][] answer = new int[0][0g];
 
         return answer;
 
