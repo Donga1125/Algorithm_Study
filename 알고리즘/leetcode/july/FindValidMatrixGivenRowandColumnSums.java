236be7feefb4cca1091d9d5b6829b0e24c4b78fe
Ernest45 <gkgkswns71@gmail.com>
2024-07-20 23:01:35 +0900

    Find Valid Matrix Given Row and Column Sums 문제추가

--- 알고리즘/leetcode/july/FindValidMatrixGivenRowandColumnSums.java
+++ 알고리즘/leetcode/july/FindValidMatrixGivenRowandColumnSums.java
@@ -1,4 +1,18 @@
 package 알고리즘.leetcode.july;
 
 public class FindValidMatrixGivenRowandColumnSums {
+
+    //1605. Find Valid Matrix Given Row and Column Sums
+
+    public static void main(String[] args) {
+
+    }
+
+    int[][] restoreMatrix(int[] rowSum, int[] colSum) {
+
+        int[][] answer = new int[][];
+
+        return answer;
+
+    }
 }
