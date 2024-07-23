0f5415c0b053bb4d103248cd7d662f819435861d
Ernest45 <gkgkswns71@gmail.com>
2024-07-23 23:46:10 +0900

    sortPeople 주석추가

--- 알고리즘/leetcode/july/SortthePeople.java
+++ 알고리즘/leetcode/july/SortthePeople.java
@@ -1,4 +1,15 @@
 package 알고리즘.leetcode.july;
 
 public class SortthePeople {
+
+
+    public static void main(String[] args) {
+
+    }
+
+    public String[] sortPeople(String[] names, int[] heights) {
+
+        return;
+
+    }
 }
