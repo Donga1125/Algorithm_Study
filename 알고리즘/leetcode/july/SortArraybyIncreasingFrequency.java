476daeed3b7672c3d7fd691d5fa3d10a9d41a24e
Ernest45 <gkgkswns71@gmail.com>
2024-07-24 18:41:16 +0900

    SortthePeople 정답

---
+++ 알고리즘/leetcode/july/SortArraybyIncreasingFrequency.java
@@ -0,0 +1,2 @@
+package 알고리즘.leetcode.july;public class SortArraybyIncreasingFrequency {
+}
--- 알고리즘/leetcode/july/SortthePeople.java
+++ 알고리즘/leetcode/july/SortthePeople.java
@@ -1,15 +1,54 @@
 package 알고리즘.leetcode.july;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class SortthePeople {
 
 
     public static void main(String[] args) {
 
+        String[] names = new String[]{"Mary", "John", "Emma"};
+        int[] heights = new int[]{180, 165, 170};
+
+        SortthePeople main = new SortthePeople();
+        main.sortPeople(names, heights);
+
+
     }
 
     public String[] sortPeople(String[] names, int[] heights) {
 
-        return;
+        // 정렬을 하는데 기준을 height이 desc로 name에 맞춰서
+
+        int length = heights.length;
+
+        Integer[] nums = new Integer[length];
+
+
+        for (int i = 0; i < length; i++) {
+            nums[i] = i;
+
+        }
+
+        Arrays.sort(nums, (a, b) -> Integer.compare(heights[b], heights[a]));
+
+
+        String[] answer = new String[length];
+
+
+        for (int i = 0; i < length; i++) {
+            answer[i] = names[nums[i]];
+        }
+
+        System.out.println(Arrays.toString(answer));
+
+
+
+            // 람다식 desc
+
+
+        return answer;
 
     }
 }
