4e9bc82b251fbee71146a32e66025e098ae4cea2
Ernest45 <gkgkswns71@gmail.com>
2024-07-26 20:10:45 +0900

    findTheCity 1334 leetcode 문제 추가

---
+++ 알고리즘/leetcode/july/FindtheCityWiththeSmallestNumberofNeighborsataThresholdDistance.java
@@ -0,0 +1,11 @@
+package 알고리즘.leetcode.july;
+
+public class FindtheCityWiththeSmallestNumberofNeighborsataThresholdDistance {
+
+    //1334. Find the City With the Smallest Number of Neighbors at a Threshold Distance
+
+
+    public int findTheCity(int n, int[][] edges, int distanceThreshold) {
+
+    }
+}
