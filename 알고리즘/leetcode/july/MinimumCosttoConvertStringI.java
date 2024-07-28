d73faab1fb0947ec337fa7d3d77c42b17ebf97c9
Ernest45 <gkgkswns71@gmail.com>
2024-07-28 22:46:59 +0900

    MinimumCosttoConvertStringI 주석 추가 문제풀이 내 방식

--- 알고리즘/leetcode/july/MinimumCosttoConvertStringI.java
+++ 알고리즘/leetcode/july/MinimumCosttoConvertStringI.java
@@ -26,6 +26,11 @@ public class MinimumCosttoConvertStringI {
 
 
     public long minimumCost(String source, String target, char[] original, char[] changed, int[] cost) {
+        // 소스를 타겟으로 바꿀려면
+        // 바꿀려는 ori에서 있는지 찾고, change로 바꾸는데 먼저 고려해야할 게 저장될 값이
+        // a->b로 가기 위해 바로 가는 게 싼가 아니면 돌아서 가는 게 싼가? 를 고려 해야함 이건
+        // 매 선택에 따라 최소 값을 정할 수 있어서 dp 배열로 관리할 수 있을 거 같다
+        // 그리고 dp 저장 전 change를 좀 더 빠르게 불러올 수 있는 정보를 저장하는게 좋을듯 ? map이라던지..
 
     }
 }
