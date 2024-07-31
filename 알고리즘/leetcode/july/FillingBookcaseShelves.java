ef7ae611a9d043de5da890a270bd6fca96dc0c8b
Ernest45 <gkgkswns71@gmail.com>
2024-07-31 19:31:03 +0900

    Filling Bookcase Shelves 문체 추가

---
+++ 알고리즘/leetcode/july/FillingBookcaseShelves.java
@@ -0,0 +1,22 @@
+package 알고리즘.leetcode.july;
+
+public class FillingBookcaseShelves {
+
+    // 1105. Filling Bookcase Shelves
+
+    //테트리스다 dp 문제인 듯
+    // 가로의 한계는 주어지고 가로에 맞게 최소의 세로를 구하는 문제
+    // 각 케이스마다 가로 세로가 주어짐
+
+    // 각 shelifWidth를 한 번 넘어가면 남은 공간 활용 x
+
+
+    public static void main(String[] args) {
+
+
+    }
+
+    public int minHeightShelves(int[][] books, int shelfWidth) {
+
+    }
+}
