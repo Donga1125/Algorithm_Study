a2e860b31221fe7ca0e51aa711a0e4a3e513e4f5
Ernest45 <gkgkswns71@gmail.com>
2024-08-02 21:49:17 +0900

    2134. Minimum Swaps to Group All 1's Together II

---
+++ 알고리즘/leetcode/august/MinimumSwapstoGroupAll1sTogetherII.java
@@ -0,0 +1,21 @@
+package 알고리즘.leetcode.august;
+
+public class MinimumSwapstoGroupAll1sTogetherII {
+
+    // 2134. Minimum Swaps to Group All 1's Together II
+
+    // 원형 배열에서 swap으로 1이 그룹되기 만들자
+
+    // 연결되어 있는 1의 길이가 있는데, 앞 뒤로도 원형이라 가능하니
+    // 앞으로 원형유지해야하나 뒤로 원형 유지 해야하나를 판별 후에 swap해야할듯?
+    // 판별을 슬라이딩 윈도를 사용하는듯
+
+    public static void main(String[] args) {
+
+
+    }
+
+    public int minSwaps(int[] nums) {
+
+    }
+}
