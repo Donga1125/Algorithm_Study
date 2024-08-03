0c855d28d43e39801ffee80d9a15505112093f01
Ernest45 <gkgkswns71@gmail.com>
2024-08-04 03:29:47 +0900

    MakeTwoArraysEqualbyReversingSubarrays

---
+++ 알고리즘/leetcode/august/MakeTwoArraysEqualbyReversingSubarrays.java
@@ -0,0 +1,21 @@
+package 알고리즘.leetcode.august;
+
+public class MakeTwoArraysEqualbyReversingSubarrays {
+
+    public static void main(String[] args) {
+
+        int[] target = new int[]{1, 2, 3, 4};
+        int[] arr = new int[]{2, 4, 1, 3};
+
+
+        MakeTwoArraysEqualbyReversingSubarrays sol = new MakeTwoArraysEqualbyReversingSubarrays();
+        sol.canBeEqual(target,arr);
+    }
+
+
+    public boolean canBeEqual(int[] target, int[] arr) {
+
+        return true;
+
+    }
+}
