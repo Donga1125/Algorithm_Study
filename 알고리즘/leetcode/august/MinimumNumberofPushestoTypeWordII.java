3ae68a42cb72012b1227d244e91fe142a8f65b7c
Ernest45 <gkgkswns71@gmail.com>
2024-08-07 19:09:04 +0900

    3016. Minimum Number of Pushes to Type Word II 효율적인 방식

--- 알고리즘/leetcode/august/MinimumNumberofPushestoTypeWordII.java
+++ 알고리즘/leetcode/august/MinimumNumberofPushestoTypeWordII.java
@@ -1,5 +1,9 @@
 package 알고리즘.leetcode.august;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 public class MinimumNumberofPushestoTypeWordII {
 
     //3016. Minimum Number of Pushes to Type Word II
@@ -11,14 +15,40 @@ public class MinimumNumberofPushestoTypeWordII {
     // 보니까 주어진 word에 확정된 키패드가 아닌
     // word가 주어지면 내가 배열을 해서 최소로 누르게 하는 것
     // 나오는 빈도가 많은 단어일수록 한번만 누르게 설계하면 될듯?
-    //우선순위를 둬서 처리하면 좋을 거 같다
+    //우선순위를 둬서 처리하면 좋을 거 같드
 
     public static void main(String[] args) {
+
+        String word = "aabbccddeeffgghhiiiiii";
+
+        MinimumNumberofPushestoTypeWordII sol = new MinimumNumberofPushestoTypeWordII();
+        sol.minimumPushes(word);
+
+
 
 
     }
 
     public int minimumPushes(String word) {
+
+        // 1숫자 세고, 숫자에 따라 8개씩 끊어서 우선순위 부여 작을수록
+        // 그럼 map에 저장할 때 key에 저장 다하고 8개 초과면 value 확인 후 우선순위 다르게
+
+        // 최고 효율의 기준은 8 8 8 2로 구성하는 것
+
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < word.length(); i++) {
+            map.put(word.charAt(i), map.getOrDefault(word.charAt(i), 0) + 1);
+
+        }
+
+        for (Map.Entry<Character, Integer> entry : map.entrySet()) {
+
+
+        }
+
+        return 1;
 
     }
 }
