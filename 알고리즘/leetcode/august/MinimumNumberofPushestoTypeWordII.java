53597a03cc1b209903ab436fde26ca25394c0d48
Ernest45 <gkgkswns71@gmail.com>
2024-08-07 18:45:36 +0900

    3016. Minimum Number of Pushes to Type Word II

---
+++ 알고리즘/leetcode/august/MinimumNumberofPushestoTypeWordII.java
@@ -0,0 +1,24 @@
+package 알고리즘.leetcode.august;
+
+public class MinimumNumberofPushestoTypeWordII {
+
+    //3016. Minimum Number of Pushes to Type Word II
+
+    //키보드의 구성이 헷갈린다 사진을 보니.. 댓글 보니까
+    // 예제 무시하고 그냥 ?
+
+    // 기본적인 일반 키패드로 생각하자는 건 아닐테고
+    // 보니까 주어진 word에 확정된 키패드가 아닌
+    // word가 주어지면 내가 배열을 해서 최소로 누르게 하는 것
+    // 나오는 빈도가 많은 단어일수록 한번만 누르게 설계하면 될듯?
+    //우선순위를 둬서 처리하면 좋을 거 같다
+
+    public static void main(String[] args) {
+
+
+    }
+
+    public int minimumPushes(String word) {
+
+    }
+}
