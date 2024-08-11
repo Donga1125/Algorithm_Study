cde9b8017f44155212e765e3edec9b079a58cb72
Ernest45 <gkgkswns71@gmail.com>
2024-08-11 16:57:52 +0900

    1508. Range Sum of Sorted Subarray Sums

---
+++ 알고리즘/leetcode/august/RangeSumofSortedSubarraySums.java
@@ -0,0 +1,18 @@
+package 알고리즘.leetcode.august;
+
+public class RangeSumofSortedSubarraySums {
+
+    //1508. Range Sum of Sorted Subarray Sums
+
+    public static void main(String[] args) {
+
+    }
+
+    public int rangeSum(int[] nums, int n, int left, int right) {
+
+        return 1;
+
+    }
+
+
+}
