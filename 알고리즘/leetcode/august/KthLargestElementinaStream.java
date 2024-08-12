3c3466b667d63de527a7095fb4951519dfbbcc53
Ernest45 <gkgkswns71@gmail.com>
2024-08-12 20:32:02 +0900

    KthLargestElementinaStream main 문

--- 알고리즘/leetcode/august/KthLargestElementinaStream.java
+++ 알고리즘/leetcode/august/KthLargestElementinaStream.java
@@ -6,4 +6,18 @@ public class KthLargestElementinaStream {
 
     // stream 안에 제일 큰 수 찾기 k
     // 정렬된 순서고, 같은 수 사이에 순서가 중요 (안정적인 정렬을 써라)
+
+    public static void main(String[] args) {
+
+        KthLargest sol = new KthLargest(3, new int[]{4, 5, 8, 2});
+        System.out.println(sol.add(3));
+        System.out.println(sol.add(5));
+        System.out.println(sol.add(10));
+        System.out.println(sol.add(9));
+        System.out.println(sol.add(4));
+
+
+
+
+    }
 }
