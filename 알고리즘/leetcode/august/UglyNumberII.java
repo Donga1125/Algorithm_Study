e87fceb5835b5f319a40ce48b24ba3cbafb29c66
Ernest45 <gkgkswns71@gmail.com>
2024-08-18 22:00:09 +0900

    264. Ugly Number II

---
+++ 알고리즘/leetcode/august/UglyNumberII.java
@@ -0,0 +1,16 @@
+package 알고리즘.leetcode.august;
+
+public class UglyNumberII {
+    //264. Ugly Number II
+    // 어글리 넘버는 2,3,5를 소인수를 가진 수이고,
+    //
+
+
+    public static void main(String[] args) {
+
+    }
+
+    public int nthUglyNumber(int n) {
+
+    }
+}
