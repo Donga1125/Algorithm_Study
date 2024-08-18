239e86b06ff26dfcb01dfb528504347ccf0fa7c0
Ernest45 <gkgkswns71@gmail.com>
2024-08-18 22:46:57 +0900

    264. Ugly Number II차례로 더해가며 정렬 미완

--- 알고리즘/leetcode/august/UglyNumberII.java
+++ 알고리즘/leetcode/august/UglyNumberII.java
@@ -1,5 +1,9 @@
 package 알고리즘.leetcode.august;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.TreeSet;
+
 public class UglyNumberII {
     //264. Ugly Number II
     // 어글리 넘버는 2,3,5를 소인수를 가진 수이고,
@@ -7,7 +11,7 @@ public class UglyNumberII {
 
     //그러면 순서대로 수 중 소수를 뺴는 건 안될까?
     // 에라토스테네스의 체로 걸러서 1,2,3,5 순서는 포함하고 그 다음 소수부터 포함 안 시키면?
-    // 실패 12도 소수인데 조합으로 가능하다 ㅠ
+    // 실패 12도 소수인데 조합으로 가능하다 ㅔ
 
 
     public static void main(String[] args) {
@@ -20,6 +24,49 @@ public class UglyNumberII {
     }
 
     public int nthUglyNumber(int n) {
+
+        // 10이라고 하면 index라는 수를 활용해 배수를 저장할 때 마다, ++해서 세어보자
+        // 막 추가하고 정렬해서 반환?
+        int index = 1;
+        int x = 1;
+        ArrayList<Integer> list = new ArrayList<>();
+        TreeSet<Integer> set = new TreeSet<>();
+        list.add(1);
+        set.add(1);
+
+        while (index < n + 1) {
+
+
+            list.add(2 * x);
+            set.add(2 * x);
+
+            list.add(3 * x);
+            set.add(3 * x);
+
+            list.add(5 * x);
+            set.add(5 * x);;
+            index += 3; // 중간에 인덱스를 넘을 수 있지만 상관 x
+            x++;
+
+        }
+
+        Collections.sort(list);
+        for (Integer i : set) {
+
+        }
+
+        return list.get(n);
+
+
+
+    }
+
+
+
+
+
+
+    private void isPrime(int n) {
 
         boolean prime[] = new boolean[1691];
 
@@ -47,7 +94,7 @@ public class UglyNumberII {
 
         }
 
-        return 0;
-
     }
+
+
 }
