2b0c58131dd6bf66cf027e60aa99871e1cf65a1d
Ernest45 <gkgkswns71@gmail.com>
2024-08-21 20:43:16 +0900

    1140. Stone Game II 주석추가

--- 알고리즘/leetcode/august/StoneGameII.java
+++ 알고리즘/leetcode/august/StoneGameII.java
@@ -3,6 +3,8 @@ package 알고리즘.leetcode.august;
 public class StoneGameII {
 
     //1140. Stone Game II
+    //엘리스와 밥이 돌 개임을 하는데 게임의 목표 가장 많은 돌로 끝나는 경우
+    // 둘 다 최적의 플레이를 할 경우 엘리스가 가질 수 있는 최대 돌을 반환
 
     public static void main(String[] args) {
 
@@ -11,5 +13,7 @@ public class StoneGameII {
 
     public int stoneGameII(int[] piles) {
 
+
     }
+
 }
