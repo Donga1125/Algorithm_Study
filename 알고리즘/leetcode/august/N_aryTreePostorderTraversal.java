fd336513d72f12afda7674992712fe4170634c97
Ernest45 <gkgkswns71@gmail.com>
2024-08-28 21:16:07 +0900

    알고리즘/leetcode/august/N_aryTreePostorderTraversal.java

--- 알고리즘/leetcode/august/N_aryTreePostorderTraversal.java
+++ 알고리즘/leetcode/august/N_aryTreePostorderTraversal.java
@@ -2,15 +2,23 @@ package 알고리즘.leetcode.august;
 
 import org.w3c.dom.Node;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class N_aryTreePostorderTraversal {
+
+    //590. N-ary Tree Postorder Traversal
 
     public static void main(String[] args) {
 
     }
 
     public List<Integer> postorder(Node root) {
+
+        List<Integer> psd = new ArrayList<>();
+
+        return psd;
+
 
     }
 
