da51d163b1c312b23e2b819c43fc26c58e5477bd
Ernest45 <gkgkswns71@gmail.com>
2024-09-01 23:13:08 +0900

    comvert1DArray

---
+++ 알고리즘/leetcode/september/Convert1DArrayInto2DArray.java
@@ -0,0 +1,14 @@
+package 알고리즘.leetcode.september;
+
+public class Convert1DArrayInto2DArray {
+
+    public static void main(String[] args) {
+
+    }
+
+    public int[][] construct2DArray(int[] original, int m, int n) {
+
+    }
+
+
+}
