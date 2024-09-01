9d789fcf57796183de3f5008c16c661898d879d4
Ernest45 <gkgkswns71@gmail.com>
2024-09-01 23:13:32 +0900

    comvert1DArray 주석추가

--- 알고리즘/leetcode/september/Convert1DArrayInto2DArray.java
+++ 알고리즘/leetcode/september/Convert1DArrayInto2DArray.java
@@ -1,6 +1,9 @@
 package 알고리즘.leetcode.september;
 
 public class Convert1DArrayInto2DArray {
+    //2022. Convert 1D Array Into 2D Array
+
+    //2차원배ㅕㄹ로 바꾸기
 
     public static void main(String[] args) {
 
