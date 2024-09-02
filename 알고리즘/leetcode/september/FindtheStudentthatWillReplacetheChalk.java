f3d3cf081850b51b47d5bcb8c144c647c08fbe04
Ernest45 <gkgkswns71@gmail.com>
2024-09-02 23:18:09 +0900

    chalkreplacer

--- 알고리즘/leetcode/september/FindtheStudentthatWillReplacetheChalk.java
+++ 알고리즘/leetcode/september/FindtheStudentthatWillReplacetheChalk.java
@@ -3,4 +3,8 @@ package 알고리즘.leetcode.september;
 public class FindtheStudentthatWillReplacetheChalk {
 
     //1894. Find the Student that Will Replace the Chalk
+
+    public int chalkReplacer(int[] chalk, int k) {
+
+    }
 }
