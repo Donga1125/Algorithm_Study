849dd6ce6507422695c1c0d6319691f0407c25f9
Ernest45 <gkgkswns71@gmail.com>
2024-09-04 11:27:11 +0900

    walking robot

---
+++ 알고리즘/leetcode/september/WalkingRobotSimulation.java
@@ -0,0 +1,12 @@
+package 알고리즘.leetcode.september;
+
+public class WalkingRobotSimulation {
+    //874. Walking Robot Simulation
+
+    public static void main(String[] args) {
+
+    }
+    public int robotSim(int[] commands, int[][] obstacles) {
+
+    }
+}
