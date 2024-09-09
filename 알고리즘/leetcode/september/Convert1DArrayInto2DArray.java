7970ba8005a8e1642579cdfa70e636d62083cfef
Ernest45 <gkgkswns71@gmail.com>
2024-09-09 20:50:21 +0900

    알고리즘/leetcode/september/Convert1DArrayInto2DArray.java

--- 알고리즘/leetcode/september/Convert1DArrayInto2DArray.java
+++ 알고리즘/leetcode/september/Convert1DArrayInto2DArray.java
@@ -1,15 +1,23 @@
 package 알고리즘.leetcode.september;
 
+import javax.swing.plaf.basic.BasicDesktopIconUI;
+
 public class Convert1DArrayInto2DArray {
     //2022. Convert 1D Array Into 2D Array
 
-    //2차원배ㅕㄹ로 바꾸기
+    //2차원배ㅕㄹ로 바꾸ㄱㄷ
 
     public static void main(String[] args) {
 
     }
 
     public int[][] construct2DArray(int[] original, int m, int n) {
+
+        int[][] arr = new int[1][1];
+
+        return arr;
+
+
 
     }
 
