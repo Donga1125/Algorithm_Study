97009daab0d7136c3eab46c3c614f381fded042f
Ernest45 <gkgkswns71@gmail.com>
2024-09-09 22:25:21 +0900

    listnode pup 오픈

--- 알고리즘/leetcode/ListNode.java
+++ 알고리즘/leetcode/ListNode.java
@@ -1,19 +1,19 @@
 package 알고리즘.leetcode;
 
 public class ListNode {
-    int val;
-    ListNode next;
+    public int val;
+    public ListNode next;
 
-    ListNode() {
+    public ListNode() {
 
     }
 
-    ListNode(int val) {
+    public ListNode(int val) {
 
         this.val = val;
     }
 
-    ListNode(int val, ListNode next) {
+    public ListNode(int val, ListNode next) {
 
         this.val = val;
         this.next = next;
