32e65e136fef66df9fac2ea8ec18713e416d83dd
Ernest45 <gkgkswns71@gmail.com>
2024-09-09 22:25:44 +0900

    SpiralMatrixIV 노드탐ㅐ

--- 알고리즘/leetcode/september/SpiralMatrixIV.java
+++ 알고리즘/leetcode/september/SpiralMatrixIV.java
@@ -1,4 +1,59 @@
 package 알고리즘.leetcode.september;
 
+import 알고리즘.leetcode.ListNode;
+
+import java.util.ArrayList;
+
 public class SpiralMatrixIV {
+
+    //2326. Spiral Matrix IV
+    // m n의 행렬이 주어지고 head의 수로 쭉 이어서 채우는데
+    // 채울 때 (나선형으로 밖에서 안으로)
+    // head의 수가 mn보다 작아지면 남은 수는 -1로 다 채운 후
+    // 행 순서 기준으로 열을 쭉 출력
+
+    public static void main(String[] args) {
+
+        int m = 3;
+        int n = 5;
+
+        ListNode node =
+                new ListNode(3,
+                        new ListNode(0,
+                                new ListNode(2, new ListNode(6, new ListNode(8, new ListNode(1, new ListNode(7, new ListNode(9, new ListNode(4, new ListNode(2,
+                                        new ListNode(5,
+                                                new ListNode(5, new ListNode(0)
+                                               ))))))))))));
+
+        SpiralMatrixIV sol = new SpiralMatrixIV();
+        sol.spiralMatrix(m, n, node);
+
+
+    }
+
+    public int[][] spiralMatrix(int m, int n, ListNode head) {
+
+        int answer[][] = new int[m][n];
+        ArrayList<Integer> arr = new ArrayList<>();
+        while (head.next == null) {
+
+
+            int now = 0;
+}
+
+        for (int i = 0; i < m; i++) {
+
+            for (int j = 0; j < n; j++) {
+                answer[i][j] = head.val;
+
+
+            }
+
+        }
+
+
+        }
+
+
+    }
 }
