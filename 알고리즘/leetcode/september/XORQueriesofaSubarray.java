5c079d5457f6aff67631ca2f8fdaf690b2b2483f
Ernest45 <gkgkswns71@gmail.com>
2024-09-13 20:43:07 +0900

    XORQueriesofaSubarray

---
+++ 알고리즘/leetcode/september/XORQueriesofaSubarray.java
@@ -0,0 +1,16 @@
+package 알고리즘.leetcode.september;
+
+public class XORQueriesofaSubarray {
+
+    //1310. XOR Queries of a Subarray
+
+    public static void main(String[] args) {
+
+    }
+
+    public int[] xorQueries(int[] arr, int[][] queries) {
+
+    }
+
+
+}
