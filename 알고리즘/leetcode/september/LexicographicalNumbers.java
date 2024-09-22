755f407994184f99b71f6fde0d4e454404a96065
Ernest45 <gkgkswns71@gmail.com>
2024-09-22 23:48:07 +0900

    386. Lexicographical Numbers

---
+++ 알고리즘/leetcode/september/LexicographicalNumbers.java
@@ -0,0 +1,17 @@
+package 알고리즘.leetcode.september;
+
+import java.util.List;
+
+public class LexicographicalNumbers {
+    //386. Lexicographical Numbers
+
+    public static void main(String[] args) {
+
+    }
+
+
+
+    public List<Integer> lexicalOrder(int n) {
+
+    }
+}
