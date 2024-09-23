ad00b1a7e793ed6d05fea8801d8252ef2513002c
Ernest45 <gkgkswns71@gmail.com>
2024-09-23 23:24:51 +0900

    2707. Extra Characters in a String 문제추가

--- 알고리즘/leetcode/september/ExtraCharactersinaString.java
+++ 알고리즘/leetcode/september/ExtraCharactersinaString.java
@@ -1,4 +1,14 @@
 package 알고리즘.leetcode.september;
 
 public class ExtraCharactersinaString {
+
+//    2707. Extra Characters in a String
+
+    public static void main(String[] args) {
+
+    }
+
+    public int minExtraChar(String s, String[] dictionary) {
+
+    }
 }
