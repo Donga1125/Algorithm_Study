45de30d326be7a9c73dafc358ab646a20c7cfd51
Ernest45 <gkgkswns71@gmail.com>
2024-09-24 23:55:07 +0900

    3043. Find the Length of the Longest Common Prefix

---
+++ 알고리즘/leetcode/september/FindtheLengthoftheLongestCommonPrefix.java
@@ -0,0 +1,13 @@
+package 알고리즘.leetcode.september;
+
+public class FindtheLengthoftheLongestCommonPrefix {
+    //3043. Find the Length of the Longest Common Prefix
+
+    public static void main(String[] args) {
+
+    }
+
+    public int longestCommonPrefix(int[] arr1, int[] arr2) {
+
+    }
+}
