0dc58bd37506bb1eada2d578e99d80b2f1d57fd1
Ernest45 <gkgkswns71@gmail.com>
2024-09-30 22:12:56 +0900

    customstackd예제추가

--- 알고리즘/leetcode/september/DesignaStackWithIncrementOperation.java
+++ 알고리즘/leetcode/september/DesignaStackWithIncrementOperation.java
@@ -1,2 +1,31 @@
-package 알고리즘.leetcode.september;public class DesignaStackWithIncrementOperation {
+package 알고리즘.leetcode.september;
+
+public class DesignaStackWithIncrementOperation {
+
+    // 1381. Design a Stack With Increment Operation
+
+
+    public static void main(String[] args) {
+
+        DesignaStackWithIncrementOperation sol = new DesignaStackWithIncrementOperation();
+
+        CustomStack asd = new CustomStack(3);
+        asd.push(1);
+        asd.push(2);
+        asd.pop();
+        asd.push(2);
+        asd.push(3);
+        asd.push(4);
+        asd.increment(5, 100);
+        asd.increment(2, 100);
+
+
+    }
+
+
+    }
+
+
+
+
 }
