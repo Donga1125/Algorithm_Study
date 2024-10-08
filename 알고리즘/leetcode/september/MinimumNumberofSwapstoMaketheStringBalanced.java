a85bce1cf74c4b849a4e8ea0a342c0588c35ea1b
Ernest45 <gkgkswns71@gmail.com>
2024-10-08 23:42:43 +0900

    Minimum Number of Swaps to Make the String Balanced 문제

--- 알고리즘/leetcode/september/MinimumNumberofSwapstoMaketheStringBalanced.java
+++ 알고리즘/leetcode/september/MinimumNumberofSwapstoMaketheStringBalanced.java
@@ -1,4 +1,12 @@
 package 알고리즘.leetcode.september;
 
 public class MinimumNumberofSwapstoMaketheStringBalanced {
+
+    public static void main(String[] args) {
+
+    }
+
+    public int minSwaps(String s) {
+
+    }
 }
