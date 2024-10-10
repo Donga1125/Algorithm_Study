f44911219a698c15dc8db6058b18c7b33fe2483b
Ernest45 <gkgkswns71@gmail.com>
2024-10-11 01:00:55 +0900

    1963. Minimum Number of Swaps to Make the String Balanced

--- 알고리즘/leetcode/september/MinimumNumberofSwapstoMaketheStringBalanced.java
+++ 알고리즘/leetcode/september/MinimumNumberofSwapstoMaketheStringBalanced.java
@@ -4,13 +4,16 @@ public class MinimumNumberofSwapstoMaketheStringBalanced {
 
     //1963. Minimum Number of Swaps to Make the String Balanced
 
-    //][][ 이런 게 들어오면 짝 맞는 게 몇개인지 리턴
+    //][][ 이런 게 들어오면 짝 맞는 게 몇개인지 리
 
     public static void main(String[] args) {
 
     }
 
     public int minSwaps(String s) {
+
+        return 3;
+
 
     }
 }
