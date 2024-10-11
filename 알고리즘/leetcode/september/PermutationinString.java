3e91f5148666c5059a3a35d602ce7935b16c41cd
Ernest45 <gkgkswns71@gmail.com>
2024-10-11 21:10:08 +0900

    정처기 테스트

--- 알고리즘/leetcode/september/PermutationinString.java
+++ 알고리즘/leetcode/september/PermutationinString.java
@@ -4,9 +4,19 @@ public class PermutationinString {
 
     public static void main(String[] args) {
 
+        PermutationinString ar = new PermutationinString();
+        ar.checkInclusion("sad", "asd");
+
     }
 
     public boolean checkInclusion(String s1, String s2) {
+
+        int i, m, f = 1;
+        for (i = 0; i < f; i++) {
+
+        }
+
+
 
         return true;
 
