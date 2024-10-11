4814f7580e3ba6c4e401f0693bf1886c99becdcf
Ernest45 <gkgkswns71@gmail.com>
2024-10-12 03:12:57 +0900

    Extra Characters in a String 리턴값만

--- 알고리즘/leetcode/september/PermutationinString.java
+++ 알고리즘/leetcode/september/PermutationinString.java
@@ -5,6 +5,13 @@ public class PermutationinString {
     public static void main(String[] args) {
 
         PermutationinString ar = new PermutationinString();
+        int s = 0;
+        for (int i = 1; i <= 100; i++) {
+            if (ar.complete(i) == 1) {
+                s += i;
+            }
+        }
+        System.out.printf("%d", s);
         ar.checkInclusion("sad", "asd");
 
     }
@@ -12,13 +19,27 @@ public class PermutationinString {
     public boolean checkInclusion(String s1, String s2) {
 
         int i, m, f = 1;
-        for (i = 0; i < f; i++) {
 
-        }
 
 
 
         return true;
 
+    }
+
+    public int complete(int n) {
+        int sum = 0;
+        for (int j = 1; j <= n/2; j++) {
+
+            if (n % j == 0) {
+                sum = sum + j;
+            }
+
+        }
+
+        if (sum == n) {
+            return 1;
+        } else
+            return 0;
     }
 }
