4bc7b4a886b18618b8f4eea14536a38f741c519f
Ernest45 <gkgkswns71@gmail.com>
2024-10-16 03:21:41 +0900

    정처기 자바 ㅈ문제

--- 알고리즘/leetcode/ppi_ipp_frint테스트.java
+++ 알고리즘/leetcode/ppi_ipp_frint테스트.java
@@ -5,7 +5,22 @@ public class ppi_ipp_frint테스트 {
     public static void main(String[] args) {
         int i = 5;
 
+        String str = "abacabcd";
+        boolean[] seen = new boolean[256];
+        System.out.print(calculFn(str, str.length()-1, seen));
 
-        System.out.printf("%d,%d,%d,%d", i++, ++i, i++, ++i);
+
+//        System.out.printf("%d,%d,%d,%d", i++, ++i, i++, ++i);
+    }
+
+    public static String calculFn(String str, int index, boolean[] seen) {
+        if(index < 0) return "";
+        char c = str.charAt(index);
+        String result = calculFn(str, index-1, seen);
+        if(!seen[c]) {
+            seen[c] = true;
+            return c + result;
+        }
+        return result;
     }
 }
