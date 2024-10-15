bac0f5c93696ebbe9dfa00d0373893e60ec4a981
Ernest45 <gkgkswns71@gmail.com>
2024-10-16 03:45:50 +0900

    정처기 자바 문제 2번

--- 알고리즘/leetcode/ppi_ipp_frint테스트.java
+++ 알고리즘/leetcode/ppi_ipp_frint테스트.java
@@ -7,14 +7,22 @@ public class ppi_ipp_frint테스트 {
 
         String str = "abacabcd";
         boolean[] seen = new boolean[256];
-        System.out.print(calculFn(str, str.length()-1, seen));
+//        System.out.print(calculFn(str, str.length()-1, seen));
+        String st1r = "ITISTESTSTRING";
+        String[] result = st1r.split("T");
+        for (String s : result) {
+            System.out.println(s);
+        }
+
 
 
 //        System.out.printf("%d,%d,%d,%d", i++, ++i, i++, ++i);
     }
 
     public static String calculFn(String str, int index, boolean[] seen) {
+
         if(index < 0) return "";
+
         char c = str.charAt(index);
         String result = calculFn(str, index-1, seen);
         if(!seen[c]) {
