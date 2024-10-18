e1b725c26a290c85f17ee39fa5b5ec5b9f67d9da
Ernest45 <gkgkswns71@gmail.com>
2024-10-18 21:52:00 +0900

    부모생성자

--- 알고리즘/leetcode/september/Parent.java
+++ 알고리즘/leetcode/september/Parent.java
@@ -1,2 +1,8 @@
-package 알고리즘.leetcode.september;public class Parent {
+package 알고리즘.leetcode.september;
+
+public class Parent {
+
+    public Parent() {
+        System.out.println("부모생성자");
+    }
 }
