44c58a4bd88a818878ffb1c051b6ab48a5c61e31
Ernest45 <gkgkswns71@gmail.com>
2024-10-18 21:51:42 +0900

    생성자 테스트

---
+++ 알고리즘/leetcode/september/Child.java
@@ -0,0 +1,2 @@
+package 알고리즘.leetcode.september;public class Childe {
+}
--- 알고리즘/leetcode/september/생성자테스트.java
+++ 알고리즘/leetcode/september/생성자테스트.java
@@ -2,23 +2,15 @@ package 알고리즘.leetcode.september;
 
 public class 생성자테스트 {
 
+    public static void main(String[] args) {
 
+        Parent a = new Parent();
 
-
-    class parent {
-
-        public parent() {
-            System.out.println("부모생성자");
-        }
     }
 
-    class child extends parent {
 
 
 
-        public child() {
-            System.out.println("자식생성자");
-        }
-    }}
+    }
 
 
