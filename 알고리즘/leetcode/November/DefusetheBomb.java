838f906cf2e65c2debf3b4c2c20e9a59a3fa82c9
Ernest45 <gkgkswns71@gmail.com>
2024-11-18 19:33:56 +0900

    DefusetheBomb

---
+++ 알고리즘/leetcode/November/DefusetheBomb.java
@@ -0,0 +1,19 @@
+package 알고리즘.leetcode.November;
+
+public class DefusetheBomb {
+
+    public static void main(String[] args) {
+
+
+        int code[] = new int[]{5, 7, 1, 4};
+        int k = 3;
+        DefusetheBomb sol = new DefusetheBomb();
+        System.out.println(sol.decrypt(code, k));
+    }
+
+
+
+    public int[] decrypt(int[] code, int k) {
+
+    }
+}
