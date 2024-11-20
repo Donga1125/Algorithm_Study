6993661c4451798275fd23ac67f43355852899d9
Ernest45 <gkgkswns71@gmail.com>
2024-11-20 19:10:43 +0900

    2516. Take K of Each Character From Left and Right

--- 알고리즘/leetcode/November/DefusetheBomb.java
+++ 알고리즘/leetcode/November/DefusetheBomb.java
@@ -2,6 +2,17 @@ package 알고리즘.leetcode.November;
 
 public class DefusetheBomb {
 
+    //1652. Defuse the Bomb
+    //원형의 배열로 이루어짐
+
+    // 주어진 k에 따라 숫자를 재배열해서 배열로 반환
+    //k > 0 크면 다음 숫자부터,차례대로 k개의 숫자들을 더해서 현재 i 번째 수로 저장
+    //k < 0 작으면 전숫자부터 차례대로 k개의 숫자들을 더해서 현재 i 수로 저장
+    //k = 0 이면 0으로 다 바꾸고 반환
+
+    // 링버퍼로 구현하면 좋을듯 모듈러로 앞뒤를 정해주자
+    // 3가지 경우 먼저 0이 들어오면 바로 빈 배열 리턴해주기
+
     public static void main(String[] args) {
 
 
@@ -14,6 +25,29 @@ public class DefusetheBomb {
 
 
     public int[] decrypt(int[] code, int k) {
+
+        int length = code.length;
+
+        int[] answer = new int[length];
+
+
+        if (k == 0) {
+            return answer;
+        }
+
+        if (k > 0) {
+
+            for (int i = 0; i < length; i++) {
+
+            }
+
+        } else {
+            k = Math.abs(k);
+
+
+        return answer;
+
+
 
     }
 }
---
+++ 알고리즘/leetcode/November/TakeKofEachCharacterFromLeftandRight.java
@@ -0,0 +1,15 @@
+package 알고리즘.leetcode.November;
+
+public class TakeKofEachCharacterFromLeftandRight {
+
+    //2516. Take K of Each Character From Left and Right
+
+    public static void main(String[] args) {
+
+
+    }
+
+    public int takeCharacters(String s, int k) {
+
+    }
+}
