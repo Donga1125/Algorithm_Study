9847c402103f1129a4810ba8c019b3dced505f0c
Ernest45 <gkgkswns71@gmail.com>
2024-11-21 20:01:19 +0900

    2516. Take K of Each Character From Left and Right

--- 알고리즘/leetcode/November/TakeKofEachCharacterFromLeftandRight.java
+++ 알고리즘/leetcode/November/TakeKofEachCharacterFromLeftandRight.java
@@ -4,6 +4,10 @@ public class TakeKofEachCharacterFromLeftandRight {
 
     //2516. Take K of Each Character From Left and Right
 
+    //문자열 s가 주어지고 abc로만 이루어져있다.
+    //k가 주어지고 abc를 k개 만큼 가져와야함
+    // 각 분마다 왼쪽 끝이나 오른쪽 끝에서 하나씩 가져올 수 있음
+    // k개 이상의 abc를 가져오기 위해서 최소시간 계산해서 반환git commit -m “2516. Take K of Each Character From Left and Right"git commit -m “2516. Take K of Each Character From Left and Right"git commit -m “2516. Take K of Each Character From Left and Right"
     public static void main(String[] args) {
 
 
