51b0fa6fdb4aaa543677e8edfa132f4aaa560b0d
Ernest45 <gkgkswns71@gmail.com>
2024-11-25 19:38:03 +0900

    3254. Find the Power of K-Size Subarrays I

---
+++ 알고리즘/leetcode/November/FindthePowerofK_SizeSubarraysI.java
@@ -0,0 +1,22 @@
+package 알고리즘.leetcode.November;
+
+public class FindthePowerofK_SizeSubarraysI {
+
+    // 3254. Find the Power of K-Size Subarrays I
+
+    public static void main(String[] args) {
+
+        int[] nums = new int[]{1, 2, 3, 4, 3, 2, 5};
+        int k = 3;
+
+        FindthePowerofK_SizeSubarraysI sol = new FindthePowerofK_SizeSubarraysI();
+        sol.resultsArray(nums, k);
+
+    }
+
+    public int[] resultsArray(int[] nums, int k) {
+
+    }
+
+
+}
