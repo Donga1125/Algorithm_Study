8085f4f9ce9a3659274e11e017bc46a11fc9da80
Ernest45 <gkgkswns71@gmail.com>
2024-11-27 20:18:16 +0900

    2924. Find Champion II

---
+++ 알고리즘/leetcode/November/FindChampionII.java
@@ -0,0 +1,14 @@
+package 알고리즘.leetcode.November;
+
+public class FindChampionII {
+
+    //2924. Find Champion II
+
+    public static void main(String[] args) {
+
+    }
+
+    public int findChampion(int n, int[][] edges) {
+
+    }
+}
