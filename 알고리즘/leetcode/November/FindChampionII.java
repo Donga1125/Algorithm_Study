b05a41e46a790eba1756561d02ca84d5ee714837
Ernest45 <gkgkswns71@gmail.com>
2024-11-27 21:11:07 +0900

    2924. Find Champion II 예제추가

--- 알고리즘/leetcode/November/FindChampionII.java
+++ 알고리즘/leetcode/November/FindChampionII.java
@@ -3,8 +3,15 @@ package 알고리즘.leetcode.November;
 public class FindChampionII {
 
     //2924. Find Champion II
+    //n개의 팀이 있고 노드가 주어짐
+    // [0,1] 이라면 1보다 0의 팀이 강한 것을 의미
+    //DAG라서 방향 노사이클 그래프
+    // 가장 쎈 팀을 반환하는데 유일하지 않다면 -1 반환
 
     public static void main(String[] args) {
+
+        int n = 3;
+        int[][] edges = new int[][]{{0, 1}, {1, 2}};
 
     }
 
