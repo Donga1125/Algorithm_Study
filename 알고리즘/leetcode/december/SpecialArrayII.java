b4c2bba2aaaf51614cee656da8621a20a800b02c
Ernest45 <gkgkswns71@gmail.com>
2024-12-09 20:55:01 +0900

    SpecialArrayII 문제

---
+++ 알고리즘/leetcode/december/SpecialArrayII.java
@@ -0,0 +1,14 @@
+package 알고리즘.leetcode.december;
+
+public class SpecialArrayII {
+
+    public static void main(String[] args) {
+        //주어진 배열에서 queries의 from부터 to 까지 부분 배열 중
+        // 서로 양 옆의 수가 짝수와 홀수의 연속이면 true 아니면 false 반환
+
+    }
+
+    public boolean[] isArraySpecial(int[] nums, int[][] queries) {
+
+    }
+}
