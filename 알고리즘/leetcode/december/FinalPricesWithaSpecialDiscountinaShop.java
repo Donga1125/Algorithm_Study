72f1bf5fb82be668a607e05cf3b38159f09213fb
Ernest45 <gkgkswns71@gmail.com>
2024-12-18 21:12:13 +0900

    1475. Final Prices With a Special Discount in a Shop n 스택활용

--- 알고리즘/leetcode/december/FinalPricesWithaSpecialDiscountinaShop.java
+++ 알고리즘/leetcode/december/FinalPricesWithaSpecialDiscountinaShop.java
@@ -1,5 +1,8 @@
 package 알고리즘.leetcode.december;
 
+import java.util.Arrays;
+import java.util.Stack;
+
 public class FinalPricesWithaSpecialDiscountinaShop {
 
     //1475. Final Prices With a Special Discount in a Shop
@@ -26,23 +29,47 @@ public class FinalPricesWithaSpecialDiscountinaShop {
     public int[] finalPrices(int[] prices) {
 
         int length = prices.length;
+        int answer[] = Arrays.copyOf(prices,length);
+        Stack<Integer> stack = new Stack<>();
 
-        for (int i = 0; i < length; i++) {
 
-            for (int j = i + 1; j < length; j++) {
+//        for (int i = 0; i < length; i++) {
+//
+//            for (int j = i + 1; j < length; j++) {
+//
+//                if (prices[i] >= prices[j]) {
+//                    prices[i] -= prices[j];
+//                    break;
+//
+//                }
+//            }
+//        }
+//        return prices;
+        // 스탹에 인덱스 넣기.
+        // 현재 수와 스택에 넣은 인덱스에 해당하는 수보다 작거나 같으면 계산 해주기
 
-                if (prices[i] >= prices[j]) {
-                    prices[i] -= prices[j];
-                    break;
+        stack.push(0);
+        for (int i = 1; i < length; i++) {
 
-                }
+            while (!stack.isEmpty() && prices[stack.peek()] >= prices[i]) {
+
+                int index = stack.pop();
+                answer[index] -= prices[i];
+
             }
+            stack.push(i);
+
+            }
+
+        while (!stack.isEmpty()) {
+
+            int index = stack.pop();
+            answer[index] = prices[index];
         }
-        return prices;
+
+        return answer;
 
 
+        }
 
-
-
-    }
 }
