99e65f72893b4a22d52b3dec22b547dbe6be62bd
Ernest45 <gkgkswns71@gmail.com>
2024-12-18 20:13:45 +0900

    1475. Final Prices With a Special Discount in a Shop n^2

--- 알고리즘/leetcode/december/FinalPricesWithaSpecialDiscountinaShop.java
+++ 알고리즘/leetcode/december/FinalPricesWithaSpecialDiscountinaShop.java
@@ -7,7 +7,7 @@ public class FinalPricesWithaSpecialDiscountinaShop {
     // price i번째를 특별 할인한다.
     // 할인 방법은 j > i j가 i보다 크고
     // prices[j] <= prices[i]
-    // 즉 오른 쪽에 있는 인덱스여야하고, 가격이 현재 i 값보다 작아야한다.
+    // 즉 오른 쪽에 있는 인덱스여야하고, 가격이 같거나 현재 i 값보다 작아야한다.
     // 여기서 가장 작은 j의 인덱스를 가진 걸로 할인이 들어감
     // 만족하지 못한다면 할인이 없다.
 
@@ -19,9 +19,30 @@ public class FinalPricesWithaSpecialDiscountinaShop {
     public static void main(String[] args) {
 
         int[] prices = new int[] {8, 4, 6, 2, 3};
+        FinalPricesWithaSpecialDiscountinaShop main = new FinalPricesWithaSpecialDiscountinaShop();
+        main.finalPrices(prices);
     }
 
     public int[] finalPrices(int[] prices) {
+
+        int length = prices.length;
+
+        for (int i = 0; i < length; i++) {
+
+            for (int j = i + 1; j < length; j++) {
+
+                if (prices[i] >= prices[j]) {
+                    prices[i] -= prices[j];
+                    break;
+
+                }
+            }
+        }
+        return prices;
+
+
+
+
 
     }
 }
