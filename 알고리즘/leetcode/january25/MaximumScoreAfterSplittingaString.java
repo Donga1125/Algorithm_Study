3392f076bb91a13c2ddae26cdf6965a23fe28b9c
Ernest45 <gkgkswns71@gmail.com>
2025-01-02 20:36:44 +0900

    1422. Maximum Score After Splitting a String

---
+++ 알고리즘/leetcode/january25/MaximumScoreAfterSplittingaString.java
@@ -0,0 +1,17 @@
+package 알고리즘.leetcode.january25;
+
+public class MaximumScoreAfterSplittingaString {
+
+    // 1422. Maximum Score After Splitting a String
+
+    public static void main(String[] args) {
+        String s = "011101";
+        MaximumScoreAfterSplittingaString sol = new MaximumScoreAfterSplittingaString();
+        sol.maxScore(s);
+    }
+
+
+    public int maxScore(String s) {
+
+    }
+}
