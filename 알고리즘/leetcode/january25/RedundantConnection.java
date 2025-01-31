81577b90e45ed307429c4d3e9600af53f46558bf
Ernest45 <gkgkswns71@gmail.com>
2025-01-31 20:38:01 +0900

    684. Redundant Connection

---
+++ 알고리즘/leetcode/january25/RedundantConnection.java
@@ -0,0 +1,18 @@
+package 알고리즘.leetcode.january25;
+
+public class RedundantConnection {
+
+    //684. Redundant Connection
+
+    public static void main(String[] args) {
+
+        int[][] edges = new int[][]{{1, 2}, {1, 3}, {2, 3}};
+        RedundantConnection sol = new RedundantConnection();
+        System.out.println(sol.findRedundantConnection(edges));
+
+    }
+
+    public int[] findRedundantConnection(int[][] edges) {
+
+    }
+}
