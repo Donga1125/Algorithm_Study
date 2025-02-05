553a22239e3d0f3df649ea541d2ca64f6e896f52
Ernest45 <gkgkswns71@gmail.com>
2025-02-05 22:23:50 +0900

    1790. Check if One String Swap Can Make Strings Equal 문제추가

--- 알고리즘/leetcode/february25/CheckifOneStringSwapCanMakeStringsEqual.java
+++ 알고리즘/leetcode/february25/CheckifOneStringSwapCanMakeStringsEqual.java
@@ -3,4 +3,13 @@ package 알고리즘.leetcode.february25;
 public class CheckifOneStringSwapCanMakeStringsEqual {
 
     //1790. Check if One String Swap Can Make Strings Equal
+
+    public static void main(String[] args) {
+
+    }
+
+
+    public boolean areAlmostEqual(String s1, String s2) {
+
+    }
 }
