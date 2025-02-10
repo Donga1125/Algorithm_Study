3de292632078f9ee3ada348376c6aaf231d8c1c7
Ernest45 <gkgkswns71@gmail.com>
2025-02-10 22:04:18 +0900

    /3174. Clear Digits

---
+++ 알고리즘/leetcode/february25/ClearDigits.java
@@ -0,0 +1,27 @@
+package 알고리즘.leetcode.february25;
+
+public class ClearDigits {
+
+    //3174. Clear Digits
+
+    // 첫번째로 등장하는 숫자를 지우고, 그 숫자에서 가장 가까운 왼쪽에 있는 숫자가 아닌 것을 지움
+    // 반복 후 남은 것을 리턴
+
+    public static void main(String[] args) {
+
+        String s = "cb34";
+        ClearDigits main = new ClearDigits();
+        main.clearDigits(s);
+    }
+
+    public String clearDigits(String s) {
+        int now = 0;
+        // 80 ~89
+
+        while (s.charAt(now) != "80") {
+
+        }
+
+    }
+}
+}
