746fe01727b2d4225090166385d5e63bf09f535d
Ernest45 <gkgkswns71@gmail.com>
2025-02-19 19:28:44 +0900

    x1415. The k-th Lexicographical String of All Happy Strings of Length n

---
+++ 알고리즘/leetcode/february25/Thek_thLexicographicalStringofAllHappyStringsofLengthn.java
@@ -0,0 +1,6 @@
+package 알고리즘.leetcode.february25;
+
+public class Thek_thLexicographicalStringofAllHappyStringsofLengthn {
+
+    // 1415. The k-th Lexicographical String of All Happy Strings of Length n
+}
