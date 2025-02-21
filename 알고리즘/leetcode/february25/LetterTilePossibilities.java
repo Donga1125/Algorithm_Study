e35861c871d11aa5ed7408d9a00c0f8787d5969f
Ernest45 <gkgkswns71@gmail.com>
2025-02-21 21:33:49 +0900

    1079. Letter Tile Possibilities

---
+++ 알고리즘/leetcode/february25/LetterTilePossibilities.java
@@ -0,0 +1,34 @@
+package 알고리즘.leetcode.february25;
+
+import 알고리즘.개발자취업하기.체스판다시칠하기.main;
+
+public class LetterTilePossibilities {
+
+    //1079. Letter Tile Possibilities
+
+    // 주어진 알파벳으로 만들 수 있는 경우의 수를 만들어라 중복 x
+    // 조건 있는 수열
+
+
+    public static void main(String[] args) {
+
+        String tiles = "AAB";
+
+        LetterTilePossibilities main = new LetterTilePossibilities();
+        main.numTilePossibilities(tiles);
+
+    }
+
+    // 조건 있는 순열 조합 만들기
+    // 조건을 검사하며 넣는 거 보다 다 만들고 중복을 제거하는 set에 담아주는 게 나을듯?
+
+
+    public int numTilePossibilities(String tiles) {
+
+    }
+
+
+    private void backTracking() {
+
+    }
+}
