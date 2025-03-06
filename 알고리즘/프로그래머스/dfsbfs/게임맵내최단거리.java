a284d125be3710ff870d3863278c24fce738be6d
Ernest45 <gkgkswns71@gmail.com>
2025-03-06 21:43:55 +0900

    게임 내 최단거리 dfs 실패 runtime error (bfs를 사용하면처음 찾는 게 최단거리임)

--- 알고리즘/프로그래머스/dfsbfs/게임맵내최단거리.java
+++ 알고리즘/프로그래머스/dfsbfs/게임맵내최단거리.java
@@ -4,7 +4,8 @@ public class 게임맵내최단거리 {
 
     //주어진 곳(length,length)까지에 도착하기 위해 최단 거리 구해라.
     //도착하지 못한다면 -1, 동서남북으로 움직일 수 있다.
-    public static int[][] movement = new int[][]{{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+    public static final int[][] dirs = new int[][]{{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+    private int minDist = Integer.MAX_VALUE;
 
     // 동남서북로 움직이는 게 효율적으로보임 1,1에 서있으니까
 
@@ -35,7 +36,10 @@ public class 게임맵내최단거리 {
 
     public int solution(int[][] maps) {
         int answer = 0;
-        boolean[] isVisited = new boolean[4];
+        int n = maps.length;
+        int m = maps[0].length;
+
+        boolean[][] isVisited = new boolean[n][m];
         // 0.동,  1.남,  2.서,  3. 북
 
 
@@ -44,31 +48,42 @@ public class 게임맵내최단거리 {
 
     }
 
-    private int dfs(int[][] maps, boolean[] isVisited, int answer, int nowX, int nowY) {
+    private void dfs(int[][] maps, boolean[][] isVisited, int answer, int nowX, int nowY) {
 
-        int last = maps.length;
+        int n = maps.length;
+        int m = maps[0].length;
 
-        if ((nowX > last || nowY > last)) {
-            answer = -1;
-            return answer;
-        }
+
             // 리턴으로 -1을 주고 바로 뛰고 싶음 배열의 범위를 벗어나면
             //
-        if (nowX == last && nowY == last) {
+        if (nowX == n-1 && nowY == m-1) {
 
-            return answer;
+            minDist = Math.min(minDist, answer);
+
+
+            return ;
         }
 
-        for (int i = 0; i < 4; i++) {
+        isVisited[nowX][nowY] = true;
+//        int[][] dirs = {{0,1}, {1,0}, {0,-1}, {-1,0}};
+        for (int[] dir : dirs) {
+            int nx = nowX + dir[0], ny = nowY + dir[1];
+            if (nx >= 0 && nx < n && ny >= 0 && ny < m && !isVisited[nx][ny] && maps[nx][ny] == 1) {
+                dfs(maps, isVisited, answer + 1,nx, ny);
+            }
+        }
+        isVisited[nowX][nowY] = false; // 백트래킹
 
             // 4가지 방향으로 움직이기
             // visited 확인 및 움직였으면 true 만들어주고,
             // 벽이거나 이미 방문한 곳은 못움직이는 예를 만들기
         }
-
-        return 1;
-
-
+//        if ((nowX > last || nowY > last)) { // 수정하자
+//            // 한번 벽이 발견되면 answer엔 -1이 리턴이니 그냥 종료시키고 싶은데
+//            answer = -1;
+//            return ; // 이건 그냥 마지막에 하면 될 거 같은데? for문 끝나고
+//            // 움직일 곳이 없을 때 or 도착했을때
+//        }
 
     }
-}
+
